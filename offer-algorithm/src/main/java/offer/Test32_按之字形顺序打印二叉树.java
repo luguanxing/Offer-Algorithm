4c1f961ce7ae6c8ae14c869f3ecd1bcfd9d62930
luguanxing <luguanxing@bigo.sg>
2020-03-31 11:23:08 +0800

    按之字形顺序打印二叉树

---
+++ offer-algorithm/src/main/java/offer/Test32_按之字形顺序打印二叉树.java
@@ -0,0 +1,68 @@
+package offer;
+
+import java.util.ArrayList;
+
+public class Test32_按之字形顺序打印二叉树 {
+
+    public static void main(String[] args) {
+        TreeNode node = new TreeNode(1);
+        TreeNode node1 = new TreeNode(2);
+        TreeNode node2 = new TreeNode(3);
+        TreeNode node11 = new TreeNode(4);
+        TreeNode node12 = new TreeNode(5);
+        TreeNode node22 = new TreeNode(6);
+        node.left = node1;
+        node.right = node2;
+        node1.left = node11;
+        node1.right = node12;
+        node2.right = node22;
+        System.out.println(new Solution().Print(node));
+    }
+
+    static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+
+
+    static class Solution {
+        private ArrayList<ArrayList<Integer>> levels = new ArrayList<>();
+
+        ArrayList<ArrayList<Integer> > Print(TreeNode pRoot) {
+            // 递归记录层级并在对应列表添加
+            levels.clear();
+            preOrder(pRoot, 0);
+            // 处理结果并返回
+            for (int i =  0; i < levels.size(); i++) {
+                if (i % 2 == 1) {
+                    // Collections.reverse(levels.get(i));
+                    ArrayList<Integer> list = levels.get(i);
+                    for (int j = 0; j < list.size() / 2; j++) {
+                        int tmp = list.get(j);
+                        list.set(j, list.get(list.size() - 1 - j));
+                        list.set(list.size() - 1 - j, tmp);
+                    }
+                }
+            }
+            return levels;
+        }
+
+        private void preOrder(TreeNode root, int level) {
+            if (root == null) {
+                return;
+            }
+            if (levels.size() <= level) {
+                levels.add(level, new ArrayList<>());
+            }
+            levels.get(level).add(root.val);
+            preOrder(root.left, level + 1);
+            preOrder(root.right, level + 1);
+        }
+    }
+
+}
