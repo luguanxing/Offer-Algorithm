c0868252b9249aa169d79afbda26d360d1ac16cb
luguanxing <luguanxing@bigo.sg>
2020-04-27 18:25:20 +0800

    分割字符串的最大得分

---
+++ offer-algorithm/src/main/java/leetcode/leetcode/contest/Test1422_分割字符串的最大得分.java
@@ -0,0 +1,40 @@
+package leetcode.leetcode.contest;
+
+public class Test1422_分割字符串的最大得分 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().maxScore("011101"));
+        System.out.println(new Solution().maxScore("00111"));
+        System.out.println(new Solution().maxScore("00"));
+    }
+
+    static class Solution {
+        public int maxScore(String s) {
+            if (s == null) {
+                return 0;
+            }
+            // 首轮计分
+            int sum = 0;
+            if (s.charAt(0) == '0') {
+                sum++;
+            }
+            for (char c : s.substring(1, s.length()).toCharArray()) {
+                if (c == '1') {
+                    sum++;
+                }
+            }
+            // 找最大值
+            int max = sum;
+            for (int i = 1; i < s.length() - 1; i++) {
+                if (s.charAt(i) == '1') {
+                    sum--;
+                } else {
+                    sum++;
+                }
+                max = Math.max(max, sum);
+            }
+            return max;
+        }
+    }
+
+}
