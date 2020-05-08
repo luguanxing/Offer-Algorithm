19422b545f128e83bbf0458f3a5eb99be20139a8
luguanxing <luguanxing@bigo.sg>
2020-05-08 11:11:51 +0800

    是否所有1都至少相隔k个元素

---
+++ offer-algorithm/src/main/java/leetcode/leetcode/contest/Test1437_是否所有1都至少相隔k个元素.java
@@ -0,0 +1,28 @@
+package leetcode.leetcode.contest;
+
+public class Test1437_是否所有1都至少相隔k个元素 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().kLengthApart(new int[]{1, 0, 0, 0, 1, 0, 0, 1}, 2));
+        System.out.println(new Solution().kLengthApart(new int[]{1, 0, 0, 1, 0, 1}, 2));
+        System.out.println(new Solution().kLengthApart(new int[]{1, 1, 1, 1, 1}, 0));
+    }
+
+    static class Solution {
+        public boolean kLengthApart(int[] nums, int k) {
+            int lastI = -1;
+            for (int i = 0; i < nums.length; i++) {
+                if (nums[i] == 1) {
+                    if (lastI != -1) {
+                        if (i - lastI <= k) {
+                            return false;
+                        }
+                    }
+                    lastI = i;
+                }
+            }
+            return true;
+        }
+    }
+
+}
