8a622e4a64ad3cfd187cd0d652b95560c15335e2
luguanxing <651571925@qq.com>
2020-06-28 10:23:19 +0800

    最接近的三数之和

--- offer-algorithm/src/main/java/leetcode/problems/Test0016_最接近的三数之和.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0016_最接近的三数之和.java
@@ -6,6 +6,7 @@ public class Test0016_最接近的三数之和 {
 
     public static void main(String[] args) {
         System.out.println(new Solution().threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
+        System.out.println(new Solution().threeSumClosest(new int[]{-3, -2, -5, 3, -4}, 1));
     }
 
     static class Solution {
@@ -13,12 +14,12 @@ public class Test0016_最接近的三数之和 {
             // 先排序
             Arrays.sort(nums);
             // 对每个元素双指针找最接近target
-            int minDiff = Integer.MAX_VALUE;
             int res = Integer.MAX_VALUE;
             for (int index = 0; index < nums.length; index++) {
                 int left = 0;
                 int right = nums.length - 1;
                 while (left < right) {
+                    // 避免左右指针和固定的元素相同
                     if (left == index) {
                         left++;
                         continue;
@@ -27,9 +28,9 @@ public class Test0016_最接近的三数之和 {
                         right--;
                         continue;
                     }
+                    // 滑动窗口找到最逼近target的数目
                     int sum = nums[index] + nums[left] + nums[right];
-                    if (Math.abs(sum - target) < minDiff) {
-                        minDiff = Math.abs(sum - target);
+                    if (Math.abs(sum - target) < Math.abs(res - target)) {
                         res = sum;
                     }
                     if (sum < target) {
