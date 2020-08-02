63451a67f32315dfcd0fbc681b2e6928c67502cf
luguanxing <651571925@qq.com>
2020-08-02 15:39:54 +0800

    排布二进制网格的最少交换次数

--- offer-algorithm/src/main/java/leetcode/contest/week200/Test5477_排布二进制网格的最少交换次数.java
+++ offer-algorithm/src/main/java/leetcode/contest/week200/Test5477_排布二进制网格的最少交换次数.java
@@ -37,7 +37,7 @@ public class Test5477_排布二进制网格的最少交换次数 {
 
     static class Solution {
         public int minSwaps(int[][] grid) {
-            // 先判断从右至左有多少个连续的0
+            // 先判断从右至左有多少个连续的0，保存起来
             int len = grid.length;
             List<Integer> list = new ArrayList<>();
             for (int[] row : grid) {
@@ -52,12 +52,13 @@ public class Test5477_排布二进制网格的最少交换次数 {
                 if (cnt == len) {
                     cnt = len - 1;
                 }
+                // 数目相同时向下弥补
                 while (list.contains(cnt)) {
                     cnt--;
                 }
                 list.add(cnt);
             }
-            // 计算排序n-1到1需要多少步
+            // 将上面的数，计算排序n-1到1需要多少步
             int res = 0;
             for (int i = 0; i < len - 1; i++) {
                 if (list.get(i) != len - 1 - i) {
