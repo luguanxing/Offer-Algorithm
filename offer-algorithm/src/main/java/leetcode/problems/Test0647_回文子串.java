6620d0f54742c7a17ab17bf5d1480003b73d15f8
luguanxing <651571925@qq.com>
2020-08-19 23:52:14 +0800

    回文子串

--- offer-algorithm/src/main/java/leetcode/problems/Test0647_回文子串.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0647_回文子串.java
@@ -1,5 +1,7 @@
 package leetcode.problems;
 
+import java.util.Arrays;
+
 public class Test0647_回文子串 {
 
     public static void main(String[] args) {
@@ -15,7 +17,7 @@ public class Test0647_回文子串 {
             for (int i = 0; i < s.length(); i++) {
                 dp[i][i] = true;
             }
-            // 动态规划，使用上一步的状态，注意因为依赖左下，所以要先上到下从循环从左到右计算
+            // 动态规划，统计所有回文可能
             int res = 0;
             for (int j = 0; j < s.length(); j++) {
                 for (int i = 0; i < s.length(); i++) {
@@ -35,6 +37,45 @@ public class Test0647_回文子串 {
                 }
             }
             return res;
+        }
+    }
+
+    static class Solution_DP {
+        public int countSubstrings(String s) {
+            // dp[i][j]表示s.substr(i,j)的回文个数
+            int[][] dp = new int[s.length()][s.length()];
+            boolean[][] isPar = new boolean[s.length()][s.length()];
+            for (int i = 0; i < s.length(); i++) {
+                dp[i][i] = 1;
+                isPar[i][i] = true;
+            }
+            // 动态规划，使用上一步的状态，注意因为依赖左下，所以要先上到下从循环从左到右计算
+            for (int i = s.length() - 1; i >= 0; i--) {
+                for (int j = 0; j < s.length(); j++) {
+                    if (i == j) {
+                        // 同一个字符
+                        dp[i][j] = 1;
+                        isPar[i][j] = true;
+                    } else if (i + 1 == j) {
+                        // 两个相邻的字符
+                        dp[i][j] = 2;
+                        if (s.charAt(i) == s.charAt(j)) {
+                            dp[i][j] += 1;
+                            isPar[i][j] = true;
+                        }
+                    } else if (j - i > 1) {
+                        // 相邻两个以上的字符
+                        dp[i][j] += dp[i + 1][j];
+                        dp[i][j] += dp[i][j - 1];
+                        dp[i][j] -= dp[i + 1][j - 1];
+                        if (s.charAt(i) == s.charAt(j) && isPar[i + 1][j - 1]) {
+                            dp[i][j] += 1;
+                            isPar[i][j] = true;
+                        }
+                    }
+                }
+            }
+            return dp[0][s.length() - 1];
         }
     }
 
