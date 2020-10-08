52314de88e802b7e2fcb0ecf54fb488423671b01
luguanxing <651571925@qq.com>
2020-10-08 13:58:53 +0800

    有序矩阵中第K小的元素

--- offer-algorithm/src/main/java/leetcode/problems/Test0378_有序矩阵中第K小的元素.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0378_有序矩阵中第K小的元素.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-public class Test378_有序矩阵中第K小的元素 {
+public class Test0378_有序矩阵中第K小的元素 {
 
     static class Solution {
         public int kthSmallest(int[][] matrix, int k) {
