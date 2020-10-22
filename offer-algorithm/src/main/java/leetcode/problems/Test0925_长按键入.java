ed2a8524e222eac8bf9a4f13b240bd843701e6ec
luguanxing <651571925@qq.com>
2020-10-22 20:48:32 +0800

    长按键入

--- offer-algorithm/src/main/java/leetcode/problems/Test0925_长按键入.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0925_长按键入.java
@@ -18,6 +18,29 @@ public class Test0925_长按键入 {
             char[] chars2 = typed.toCharArray();
             int index1 = 0;
             int index2 = 0;
+            // 遍历一遍typed字符串
+            while (index2 < chars2.length) {
+                if (index1 < chars1.length && chars1[index1] == chars2[index2]) {
+                    index1++;
+                    index2++;
+                } else if (index2 > 0 && chars2[index2 - 1] == chars2[index2]) {
+                    index2++;
+                } else {
+                    return false;
+                }
+            }
+            // 判断name字符串是否匹配完
+            return index1 == chars1.length;
+        }
+    }
+
+
+    static class Solution_未优化 {
+        public boolean isLongPressedName(String name, String typed) {
+            char[] chars1 = name.toCharArray();
+            char[] chars2 = typed.toCharArray();
+            int index1 = 0;
+            int index2 = 0;
             while (index1 < chars1.length && index2 < chars2.length) {
                 char c1 = chars1[index1];
                 char c2 = chars2[index2];
