f24863aa0a93179567437a49c19d968543e89f20
luguanxing <651571925@qq.com>
2020-11-02 12:16:03 +0800

    可以到达的最远建筑

--- offer-algorithm/src/main/java/leetcode/contest/week213/Test5556_可以到达的最远建筑.java
+++ offer-algorithm/src/main/java/leetcode/contest/week213/Test5556_可以到达的最远建筑.java
@@ -1,5 +1,8 @@
 package leetcode.contest.week213;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class Test5556_可以到达的最远建筑 {
 
     public static void main(String[] args) {
@@ -12,9 +15,36 @@ public class Test5556_可以到达的最远建筑 {
         System.out.println(new Solution().furthestBuilding(
                 new int[]{14, 3, 19, 3}, 17, 0
         ));
+        System.out.println(new Solution().furthestBuilding(
+                new int[]{1, 5, 6, 7, 8}, 4, 1
+        ));
+        System.out.println(new Solution().furthestBuilding(
+                new int[]{4, 2, 7, 6, 9, 14, 12}, 5, 1
+        ));
     }
 
     static class Solution {
+        public int furthestBuilding(int[] heights, int bricks, int ladders) {
+            PriorityQueue<Integer> diffs = new PriorityQueue<>(Comparator.naturalOrder());
+            int currentBricks = 0;
+            for (int i = 1; i < heights.length; i++) {
+                int diff = heights[i] - heights[i - 1];
+                if (diff <= 0) {
+                    continue;
+                }
+                diffs.add(diff);
+                if (diffs.size() > ladders) {
+                    currentBricks += diffs.poll();
+                }
+                if (currentBricks > bricks) {
+                    return i - 1;
+                }
+            }
+            return heights.length - 1;
+        }
+    }
+
+    static class Solution_错误 {
         public int furthestBuilding(int[] heights, int bricks, int ladders) {
             for (int i = 0; i < heights.length - 1; i++) {
                 int current = heights[i];
