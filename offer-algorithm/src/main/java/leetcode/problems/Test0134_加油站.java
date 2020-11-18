7446cb8bfb24b85e314dddf48103ee70a1463e1b
luguanxing <651571925@qq.com>
2020-11-18 21:00:56 +0800

    加油站

---
+++ offer-algorithm/src/main/java/leetcode/problems/Test0134_加油站.java
@@ -0,0 +1,71 @@
+package leetcode.problems;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Test0134_加油站 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution_优化().canCompleteCircuit(
+                new int[]{1, 2, 3, 4, 5},
+                new int[]{3, 4, 5, 1, 2}
+        ));
+        System.out.println(new Solution_优化().canCompleteCircuit(
+                new int[]{2, 3, 4},
+                new int[]{3, 4, 3}
+        ));
+    }
+
+    static class Solution_优化 {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            // 如果总油量小于总消耗无法走完
+            int left = Arrays.stream(gas).sum() - Arrays.stream(cost).sum();
+            if (left < 0) {
+                return -1;
+            }
+            // 计算每步能向后多少，当前点如果走不过去则绕后，因为left>0最后必能走完
+            int len = gas.length;
+            int[] forward = new int[len];
+            for (int i = 0; i < len; i++) {
+                forward[i] = gas[i] - cost[i];
+            }
+            int start = 0;
+            int sum = 0;
+            for (int i = 0; i < len; i++) {
+                sum += forward[i];
+                if (sum < 0) {
+                    start = i + 1;
+                    sum = 0;
+                }
+            }
+            return start;
+        }
+    }
+
+    static class Solution {
+        public int canCompleteCircuit(int[] gas, int[] cost) {
+            int len = gas.length;
+            List<Integer> forwardList = new ArrayList<>();
+            for (int i = 0; i < len; i++) {
+                forwardList.add(gas[i] - cost[i]);
+            }
+            // 从i开始看看能不能连续走len步
+            forwardList.addAll(forwardList);
+            for (int i = 0; i < len; i++) {
+                int sum = 0;
+                for (int j = i; j < i + len; j++) {
+                    sum += forwardList.get(j);
+                    if (sum < 0) {
+                        break;
+                    }
+                }
+                if (sum >= 0) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+
+}
