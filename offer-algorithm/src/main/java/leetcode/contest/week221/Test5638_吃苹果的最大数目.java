ec61d831a5e2f1e4795a7d72de02387b836e8dd5
luguanxing <651571925@qq.com>
2020-12-27 16:45:34 +0800

    吃苹果的最大数目

--- offer-algorithm/src/main/java/leetcode/contest/week221/Test5638_吃苹果的最大数目.java
+++ offer-algorithm/src/main/java/leetcode/contest/week221/Test5638_吃苹果的最大数目.java
@@ -17,59 +17,42 @@ public class Test5638_吃苹果的最大数目 {
                 new int[]{9, 10, 1, 7, 0, 2, 1, 4, 1, 7, 0, 11, 0, 11, 0, 0, 9, 11, 11, 2, 0, 5, 5},
                 new int[]{3, 19, 1, 14, 0, 4, 1, 8, 2, 7, 0, 13, 0, 13, 0, 0, 2, 2, 13, 1, 0, 3, 7}
         ));
+        System.out.println(new Solution().eatenApples(
+                new int[]{2, 2, 2, 2, 2},
+                new int[]{10, 8, 6, 4, 2}
+        ));
     }
 
     static class Solution {
         public int eatenApples(int[] apples, int[] days) {
-            int MAX = 20005;
-            int[] canEat = new int[MAX];
-            for (int i = 0; i < apples.length; i++) {
-                int apple = apples[i];
-                int day = days[i];
-                for (int j = 0; j < day && apple > 0; j++) {
-                    if (canEat[i + j] == 0) {
-                        canEat[i + j] = 1;
-                        apple--;
-                    }
-                }
-            }
-            return Arrays.stream(canEat).sum();
-        }
-    }
-
-    static class Solution_模拟 {
-        public int eatenApples(int[] apples, int[] days) {
             int res = 0;
-            Set<Stat> stats = new TreeSet<>(Comparator.comparingInt(o -> o.days));
+            Set<int[]> stats = new TreeSet<>(Comparator.comparingInt(stat -> stat[1]));
             // 模拟每天情况
             int index = 0;
             do {
                 // 每天加入新苹果
                 if (index < apples.length && apples[index] > 0) {
-                    Stat stat = new Stat(apples[index], days[index]);
-                    stats.add(stat);
+                    stats.add(new int[]{apples[index], index + days[index]});
                 }
                 index++;
                 // 吃掉快到期的那个苹果
                 if (!stats.isEmpty()) {
-                    Stat willEat = stats.iterator().next();
-                    willEat.apple--;
+                    int[] stat = stats.iterator().next();
+                    stat[0]--;
                     res++;
                 }
                 // 更新吃掉的苹果和过期时间
-                stats.removeIf(stat -> stat.apple <= 0 || --stat.days <= 0);
+                Iterator<int[]> iterator = stats.iterator();
+                while (iterator.hasNext()) {
+                    int[] stat = iterator.next();
+                    if (stat[0] <= 0 || stat[1] <= index) {
+                        iterator.remove();
+                    } else {
+                        break;
+                    }
+                }
             } while (!stats.isEmpty() || index < days.length);
             return res;
-        }
-
-        class Stat {
-            int apple;
-            int days;
-
-            public Stat(int apple, int days) {
-                this.apple = apple;
-                this.days = days;
-            }
         }
     }
 
