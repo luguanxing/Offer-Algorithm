0d5a52e6a5b244176ae05866f7a8ae9bb456bc4e
luguanxing <651571925@qq.com>
2021-01-01 16:01:06 +0800

    无重叠区间

---
+++ offer-algorithm/src/main/java/leetcode/problems/Test0435_无重叠区间.java
@@ -0,0 +1,46 @@
+package leetcode.problems;
+
+import java.util.Arrays;
+
+public class Test0435_无重叠区间 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().eraseOverlapIntervals(
+                new int[][]{{1, 2}, {2, 3}, {3, 4}, {1, 3}}
+        ));
+        System.out.println(new Solution().eraseOverlapIntervals(
+                new int[][]{{1, 2}, {1, 2}, {1, 2}}
+        ));
+        System.out.println(new Solution().eraseOverlapIntervals(
+                new int[][]{{1, 2}, {2, 3}}
+        ));
+    }
+
+    static class Solution {
+        public int eraseOverlapIntervals(int[][] intervals) {
+            if (intervals.length == 0) {
+                return 0;
+            }
+            // 排序:先start小的在前，再按end小的在前排序
+            Arrays.sort(intervals, (o1, o2) -> {
+                if (o1[0] == o2[0]) {
+                    return o1[1] - o2[1];
+                } else {
+                    return o1[0] - o2[0];
+                }
+            });
+            // 每次选择和上一个不重合的结尾最短的段放入，计算能合并成多少段
+            int max = 1;
+            int end = intervals[0][1];
+            for (int i = 1; i < intervals.length; i++) {
+                int start = intervals[i][0];
+                if (end <= start) {
+                    end = intervals[i][1];
+                    max++;
+                }
+            }
+            return intervals.length - max;
+        }
+    }
+
+}
