647f4c810e4d8f32fefc7e8b45a5dca0b91ef39f
luguanxing <651571925@qq.com>
2021-03-16 20:47:24 +0800

    螺旋矩阵II

--- offer-algorithm/src/main/java/leetcode/problems/Test0059_螺旋矩阵II.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0059_螺旋矩阵II.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Test0059_螺旋矩阵II {
 
     public static void main(String[] args) {
-//        System.out.println(Arrays.toString(new Solution().generateMatrix(3)));
+        System.out.println(Arrays.toString(new Solution().generateMatrix(3)));
         System.out.println(Arrays.toString(new Solution().generateMatrix(4)));
     }
 
