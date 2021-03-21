e2cd3db6c1cd1a82f4e950273ffc6f1f95f5169c
luguanxing <651571925@qq.com>
2021-03-21 17:21:43 +0800

    有界数组中指定下标处的最大值

--- offer-algorithm/src/main/java/leetcode/contest/week233/Test5711_有界数组中指定下标处的最大值.java
+++ offer-algorithm/src/main/java/leetcode/contest/week233/Test5711_有界数组中指定下标处的最大值.java
@@ -10,24 +10,29 @@ public class Test5711_有界数组中指定下标处的最大值 {
 
     static class Solution {
         public int maxValue(int n, int index, int maxSum) {
-            if (maxSum <= n) {
-                return 1;
-            }
-            // 每位先设置1，剩下从1,2,3堆积到maxSum
+            // 过程模拟，从index开始往两边扩充：维护一个[l,r]范围
+            // 每次往范围内每个位置+1，通过这种方式维护一个向上生长的“三角形
+            int left = index;
+            int right = index;
+            // 整个数组一开始全部填充为1
+            int res = 1;
             maxSum -= n;
-            int max = 0;
-            for (int i = 1; i <= 10005; i++) {
-                if (i * (i + 1) / 2 >= maxSum) {
-                    max = i;
+            while (0 < left || right < n - 1) {
+                int len = right - left + 1;
+                if (maxSum >= len) {
+                    // 当前[l,r]范围全部+1
+                    res++;
+                    maxSum -= len;
+                    // 往左右两边扩
+                    left = Math.max(0, left - 1);
+                    right = Math.min(right + 1, n - 1);
+                } else {
                     break;
                 }
             }
-            int delta = max * (max + 1) / 2 - maxSum;
-            if (delta == 0) {
-                return  Math.max(max, index) + 1;
-            } else {
-                return Math.max(max, index);
-            }
+            // 扩大到整个数组之后，剩余的值“雨露均沾”一下
+            res += maxSum / n;
+            return res;
         }
     }
 
