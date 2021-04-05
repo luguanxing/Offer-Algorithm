7d2abcdd0b16f2755099233a26fd6af584979310
luguanxing <651571925@qq.com>
2021-04-05 17:56:23 +0800

    双指针

--- offer-algorithm/src/main/java/leetcode/contest/year2021/spring/Test1_采购方案.java
+++ offer-algorithm/src/main/java/leetcode/contest/year2021/spring/Test1_采购方案.java
@@ -26,7 +26,58 @@ public class Test1_采购方案 {
                 new int[]{1, 1, 1, 1},
                 3
         ));
+        System.out.println(new Solution().purchasePlans(
+                new int[]{1, 1, 2, 2},
+                5
+        ));
+        System.out.println(new Solution_双指针().purchasePlans(
+                new int[]{1, 1, 2, 2},
+                5
+        ));
     }
+
+    static class Solution_双指针 {
+        public int purchasePlans(int[] nums, int target) {
+            Arrays.sort(nums);
+            int left = 0;
+            int right = nums.length - 1;
+            int res = 0;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum > target) {
+                    right--;
+                    continue;
+                }
+                res += right - left;
+                res %= 1000000007;
+                left++;
+            }
+            return res;
+        }
+    }
+
+//    static class Solution_双指针 {
+//        public int purchasePlans(int[] nums, int target) {
+//            int mod = 1_000_000_007;
+//            int ans = 0;
+//            // 首先对整体进行排序
+//            Arrays.sort(nums);
+//            // 双指针，left 从前往后找，right 从后往前
+//            int left = 0, right = nums.length - 1;
+//            while (left < right) {
+//                // 如果当前左右之和大于了目标值，说明偏大了，就把右指针往左移动
+//                if (nums[left] + nums[right] > target) right--;
+//                else {
+//                    // 否则的话，说明找到了合适的，需要把两者中间的元素个数都累加起来
+//                    ans += right - left;
+//                    // 然后再向右移动左指针
+//                    left++;
+//                }
+//                ans %= mod;
+//            }
+//            return ans % mod;
+//        }
+//    }
 
     static class Solution {
         public int purchasePlans(int[] nums, int target) {
