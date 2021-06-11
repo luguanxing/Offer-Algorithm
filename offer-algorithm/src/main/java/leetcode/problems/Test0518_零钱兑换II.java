b1d80303607b2ab6b3bfd34220aeea9853517584
luguanxing <651571925@qq.com>
2021-06-11 17:09:20 +0800

    零钱兑换II

--- offer-algorithm/src/main/java/leetcode/problems/Test0518_零钱兑换II.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0518_零钱兑换II.java
@@ -10,6 +10,32 @@ public class Test0518_零钱兑换II {
 
     static class Solution {
         public int change(int amount, int[] coins) {
+            // dp[i][j]表示使用前i枚硬币凑出j元的方法数
+            // dp[i][j] = dp[i-1][j] + dp[i][j-coins[i]]
+            int[][] dp = new int[coins.length + 1][amount + 1];
+
+            // 不凑钱的方法只有1种
+            for (int i = 0; i <= coins.length; i++) {
+                dp[i][0] = 1;
+            }
+
+            for (int i = 1; i <= coins.length; i++) {
+                for (int j = 1; j <= amount; j++) {
+                    int coin = coins[i - 1];
+                    // 不用第i枚硬币凑出了j元
+                    dp[i][j] += dp[i - 1][j];
+                    // 能用且用第i枚硬币凑出了j元
+                    if (j >= coin) {
+                        dp[i][j] += dp[i][j - coin];
+                    }
+                }
+            }
+            return dp[coins.length][amount];
+        }
+    }
+
+    static class Solution_ {
+        public int change(int amount, int[] coins) {
             // dp[i][j]表示用前i枚硬币凑出j元的方法
             int[][] dp = new int[coins.length + 1][amount + 1];
             for (int i = 0; i <= coins.length; i++) {
