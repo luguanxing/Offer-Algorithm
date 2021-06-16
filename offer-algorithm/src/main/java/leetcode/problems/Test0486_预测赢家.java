f5317987990cb35e967b84f2aa9ed536cb9d047f
luguanxing <651571925@qq.com>
2021-06-16 20:25:39 +0800

    预测赢家

--- offer-algorithm/src/main/java/leetcode/problems/Test0486_预测赢家.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0486_预测赢家.java
@@ -1,9 +1,5 @@
 package leetcode.problems;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class Test0486_预测赢家 {
 
     public static void main(String[] args) {
@@ -14,38 +10,42 @@ public class Test0486_预测赢家 {
 
     static class Solution {
         public boolean PredictTheWinner(int[] nums) {
-            List<Integer> numList = Arrays.stream(nums).boxed().collect(Collectors.toList());
-            int maxGet = check(numList, true, 0);
-            return maxGet >= 0;
+            // dp[i][j]表示i,j取范围之差最大的数
+            int len = nums.length;
+            int[][] dp = new int[len][len];
+            for (int i = 0; i < len; i++) {
+                dp[i][i] = nums[i];
+            }
+            // dp[i][j] = max(nums[i]-dp[i+1][j], nums[j]-dp[i][j-1])
+            for (int i = len - 2; i >= 0; i--) {
+                for (int j = i + 1; j < len; j++) {
+                    dp[i][j] = Math.max(nums[i] - dp[i + 1][j], nums[j] - dp[i][j - 1]);
+                }
+            }
+            return dp[0][len - 1] >= 0;
+        }
+    }
+
+    static class Solution_递归 {
+        public boolean PredictTheWinner(int[] nums) {
+            // 判断Alice最赚的最大分数扣掉bob赚的最大分数是否大于0
+            return check(nums, 0, nums.length - 1, true) >= 0;
         }
 
-        private int check(List<Integer> numList, boolean first, int sum) {
-            if (numList.size() == 1) {
-                if (first) {
-                    return sum + numList.get(0);
-                } else {
-                    return sum - numList.get(0);
-                }
+        private int check(int[] nums, int start, int end, boolean isAlice) {
+            if (start == end) {
+                return nums[start] * (isAlice ? 1 : -1);
+            }
+            if (isAlice) {
+                // Alice本轮尽可能拿最大的数
+                int aliceTakeFirst = nums[start] + check(nums, start + 1, end, !isAlice);
+                int aliceTakeLast = nums[end] + check(nums, start, end - 1, !isAlice);
+                return Math.max(aliceTakeFirst, aliceTakeLast);
             } else {
-                int left = numList.get(0);
-                int right = numList.get(numList.size() - 1);
-                if (first) {
-                    numList.remove(0);
-                    int takeLeft = check(numList, !first, sum + left);
-                    numList.add(0, left);
-                    numList.remove(numList.size() - 1);
-                    int takeRight = check(numList, !first, sum + right);
-                    numList.add(numList.size(), right);
-                    return Math.max(takeLeft, takeRight);
-                } else {
-                    numList.remove(0);
-                    int takeLeft = check(numList, !first, sum - left);
-                    numList.add(0, left);
-                    numList.remove(numList.size() - 1);
-                    int takeRight = check(numList, !first, sum - right);
-                    numList.add(numList.size(), right);
-                    return Math.min(takeLeft, takeRight);
-                }
+                // Bob本轮尽可能拿最小的数
+                int bobTakeFirst = nums[start] * -1 + check(nums, start + 1, end, !isAlice);
+                int bobTakeLast = nums[end] * -1 + check(nums, start, end - 1, !isAlice);
+                return Math.min(bobTakeFirst, bobTakeLast);
             }
         }
     }
