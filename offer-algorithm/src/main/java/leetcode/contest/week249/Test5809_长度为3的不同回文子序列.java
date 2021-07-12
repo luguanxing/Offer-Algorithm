7124d4bfe4aef86cb4f914aa8cdf05b47ad1a55c
luguanxing <651571925@qq.com>
2021-07-13 00:20:25 +0800

    长度为 3 的不同回文子序列

--- offer-algorithm/src/main/java/leetcode/contest/week249/Test5809_长度为3的不同回文子序列.java
+++ offer-algorithm/src/main/java/leetcode/contest/week249/Test5809_长度为3的不同回文子序列.java
@@ -5,11 +5,33 @@ import java.util.*;
 public class Test5809_长度为3的不同回文子序列 {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().countPalindromicSubsequence("aabca"));
-        System.out.println(new Solution().countPalindromicSubsequence("adc"));
-        System.out.println(new Solution().countPalindromicSubsequence("bbcbaba"));
-        System.out.println(new Solution().countPalindromicSubsequence("uuuuu"));
-        System.out.println(new Solution().countPalindromicSubsequence("tlpjzdmtwderpkpmgoyrcxttiheassztncqvnfjeyxxp"));
+        System.out.println(new Solution_优化().countPalindromicSubsequence("aabca"));
+        System.out.println(new Solution_优化().countPalindromicSubsequence("adc"));
+        System.out.println(new Solution_优化().countPalindromicSubsequence("bbcbaba"));
+        System.out.println(new Solution_优化().countPalindromicSubsequence("uuuuu"));
+        System.out.println(new Solution_优化().countPalindromicSubsequence("tlpjzdmtwderpkpmgoyrcxttiheassztncqvnfjeyxxp"));
+    }
+
+    static class Solution_优化 {
+        public int countPalindromicSubsequence(String s) {
+            int[] left = new int[26];
+            int[] right = new int[26];
+            for (int i = 0; i < s.length(); i++) {
+                right[s.charAt(i) - 'a'] = i;
+            }
+            for (int i = s.length() - 1; i >= 0; i--) {
+                left[s.charAt(i) - 'a'] = i;
+            }
+            int res = 0;
+            for (char c = 'a'; c <= 'z'; c++) {
+                Set<Character> set = new HashSet<>();
+                for (int i = left[c - 'a'] + 1; i < right[c - 'a']; i++) {
+                    set.add(s.charAt(i));
+                }
+                res += set.size();
+            }
+            return res;
+        }
     }
 
     static class Solution {
@@ -24,7 +46,7 @@ public class Test5809_长度为3的不同回文子序列 {
                     int current = 0;
                     for (int j = list.get(list.size() - 1) + 1; j < i; j++) {
                         char m = s.charAt(j);
-                        Set<Character> set = visited.getOrDefault(c , new HashSet<>());
+                        Set<Character> set = visited.getOrDefault(c, new HashSet<>());
                         if (!set.contains(m)) {
                             set.add(m);
                             current++;
