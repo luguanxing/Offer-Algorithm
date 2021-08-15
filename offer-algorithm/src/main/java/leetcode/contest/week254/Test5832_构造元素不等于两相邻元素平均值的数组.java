bf77d2e699b21c37cbcc164e196fed44b4d2c4ab
luguanxing <651571925@qq.com>
2021-08-15 11:05:45 +0800

    构造元素不等于两相邻元素平均值的数组

---
+++ offer-algorithm/src/main/java/leetcode/contest/week254/Test5832_构造元素不等于两相邻元素平均值的数组.java
@@ -0,0 +1,38 @@
+package leetcode.contest.week254;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Test5832_构造元素不等于两相邻元素平均值的数组 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().rearrangeArray(new int[]{1, 2, 3, 4, 5})));
+        System.out.println(Arrays.toString(new Solution().rearrangeArray(new int[]{6, 2, 0, 9, 7})));
+        System.out.println(Arrays.toString(new Solution().rearrangeArray(new int[]{1, 2, 3})));
+        System.out.println(Arrays.toString(new Solution().rearrangeArray(new int[]{0, 4, 1, 5, 3})));
+    }
+
+    static class Solution {
+        public int[] rearrangeArray(int[] nums) {
+            int len = nums.length;
+            boolean isOk = false;
+            while (!isOk) {
+                for (int i = 1; i < len - 1; i++) {
+                    int prev = nums[i - 1];
+                    int mid = nums[i];
+                    int next = nums[i + 1];
+                    if (mid * 2 == prev + next) {
+                        int tmp = nums[len - 1];
+                        nums[len - 1] = mid;
+                        nums[i] = tmp;
+                    }
+                }
+                isOk = true;
+            }
+            return nums;
+        }
+    }
+
+}
