c40b2b14d3a2482bc57ec0c7a74a8374fc5d5d52
luguanxing <651571925@qq.com>
2021-11-14 17:15:34 +0800

    全排列

--- offer-algorithm/src/main/java/leetcode/problems/Test0046_全排列.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0046_全排列.java
@@ -1,80 +1,39 @@
 package leetcode.problems;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Test0046_全排列 {
 
     public static void main(String[] args) {
         System.out.println(new Solution().permute(new int[]{1, 2, 3}));
         System.out.println(new Solution().permute(new int[]{}));
-        System.out.println(new Solution().permute(null));
     }
 
     static class Solution {
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        List<Integer> left = new ArrayList<>();
+
         public List<List<Integer>> permute(int[] nums) {
-            if (nums == null) {
-                return null;
-            }
-            // 初始化
-            result.clear();
-            current.clear();
-            left.clear();
-            for (int num : nums) {
-                left.add(num);
-            }
-            // 回溯
-            permute(current, left);
+            List<Integer> last = Arrays.stream(nums).boxed().collect(Collectors.toList());
+            List<Integer> current = new ArrayList<>();
+            dfs(last, current);
             return result;
         }
 
-        public void permute(List<Integer> current, List<Integer> left) {
-            if (left.size() == 0) {
+        private void dfs(List<Integer> last, List<Integer> current) {
+            if (last.isEmpty()) {
                 result.add(new ArrayList<>(current));
                 return;
             }
-            for (int i = 0; i < left.size(); i++) {
-                Integer num = left.get(i);
-                left.remove(i);
-                current.add(num);
-                permute(current, left);
-                current.remove(current.size() - 1);
-                left.add(i, num);
-            }
-        }
-    }
-
-    static class Solution_DFS {
-        List<List<Integer>> result = new ArrayList<>();
-
-        public List<List<Integer>> permute(int[] nums) {
-            if (nums == null) {
-                return null;
-            }
-            result.clear();
-            List<Integer> numList = new ArrayList<>();
-            for (int num : nums) {
-                numList.add(num);
-            }
-            permute(new ArrayList<>(), numList);
-            return result;
-        }
-
-        public void permute(List<Integer> current, List<Integer> left) {
-            if (left.size() == 0) {
-                result.add(new ArrayList<>(current));
-                return;
-            }
-            for (int i = 0; i < left.size(); i++) {
-                Integer nextNum = left.get(i);
-                ArrayList<Integer> nextCurrent = new ArrayList<>(current);
-                ArrayList<Integer> nextLeft = new ArrayList<>(left);
-                nextCurrent.add(nextNum);
-                nextLeft.remove(i);
-                permute(nextCurrent, nextLeft);
+            for (Integer num : last) {
+                // 从last选择num，一定要选择出一个，没有不选择的情况
+                List<Integer> newLast = new ArrayList<>(last);
+                List<Integer> newCurrent = new ArrayList<>(current);
+                newLast.remove(num);
+                newCurrent.add(num);
+                dfs(newLast, newCurrent);
             }
         }
     }
