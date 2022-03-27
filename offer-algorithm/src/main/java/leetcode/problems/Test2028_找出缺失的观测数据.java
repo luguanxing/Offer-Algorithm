9d1a78eb0ec4a6b26d34b15553e8a2572305b0a1
luguanxing <651571925@qq.com>
2022-03-27 11:18:21 +0800

    找出缺失的观测数据

---
+++ offer-algorithm/src/main/java/leetcode/problems/Test2028_找出缺失的观测数据.java
@@ -0,0 +1,51 @@
+package leetcode.problems;
+
+import java.util.Arrays;
+
+public class Test2028_找出缺失的观测数据 {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().missingRolls(
+                new int[]{3, 2, 4, 3}, 4, 2
+        )));
+        System.out.println(Arrays.toString(new Solution().missingRolls(
+                new int[]{1, 5, 6}, 3, 4
+        )));
+        System.out.println(Arrays.toString(new Solution().missingRolls(
+                new int[]{1, 2, 3, 4}, 6, 4
+        )));
+        System.out.println(Arrays.toString(new Solution().missingRolls(
+                new int[]{1}, 3, 1
+        )));
+        System.out.println(Arrays.toString(new Solution().missingRolls(
+                new int[]{6, 3, 4, 3, 5, 3}, 1, 6
+        )));
+        System.out.println(Arrays.toString(new Solution().missingRolls(
+                new int[]{4, 5, 6, 2, 3, 6, 5, 4, 6, 4, 5, 1, 6, 3, 1, 4, 5, 5, 3, 2, 3, 5, 3, 2, 1, 5, 4, 3, 5, 1, 5}, 4, 40
+        )));
+        System.out.println(Arrays.toString(new Solution().missingRolls(
+                new int[]{4, 2, 2, 5, 4, 5, 4, 5, 3, 3, 6, 1, 2, 4, 2, 1, 6, 5, 4, 2, 3, 4, 2, 3, 3, 5, 4, 1, 4, 4, 5, 3, 6, 1, 5, 2, 3, 3, 6, 1, 6, 4, 1, 3}, 2, 53
+        )));
+    }
+
+    static class Solution {
+        public int[] missingRolls(int[] rolls, int mean, int n) {
+            int rollLen = rolls.length;
+            int rollSum = Arrays.stream(rolls).sum();
+            int sum = mean * (n + rollLen);
+            // 需要用n个数凑出leftSum
+            int leftSum = sum - rollSum;
+            if (leftSum < 0 || n * 6 < leftSum || n > leftSum) {
+                return new int[]{};
+            }
+            int[] res = new int[n];
+            Arrays.fill(res, leftSum / n);
+            leftSum = leftSum - Arrays.stream(res).sum();
+            for (int i = 0; i < leftSum; i++) {
+                res[i]++;
+            }
+            return res;
+        }
+    }
+
+}
