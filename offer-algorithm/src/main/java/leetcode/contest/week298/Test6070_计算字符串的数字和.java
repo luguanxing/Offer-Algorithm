b1b258cdd44b401fdf71aa6bf07e45f66e2805c7
luguanxing <651571925@qq.com>
2022-04-17 10:40:56 +0800

    计算字符串的数字和

---
+++ offer-algorithm/src/main/java/leetcode/contest/week298/Test6070_计算字符串的数字和.java
@@ -0,0 +1,34 @@
+package leetcode.contest.week298;
+
+public class Test6070_计算字符串的数字和 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().digitSum("11111222223", 3));
+        System.out.println(new Solution().digitSum("00000000", 3));
+    }
+
+    static class Solution {
+        public String digitSum(String s, int k) {
+            while (s.length() > k) {
+                String newS = "";
+                while (s.length() > k) {
+                    String subS = s.substring(0, k);
+                    s = s.substring(k);
+                    int subSsum = 0;
+                    for (char c : subS.toCharArray()) {
+                        subSsum += c - '0';
+                    }
+                    newS += subSsum;
+                }
+                int subSsum = 0;
+                for (char c : s.toCharArray()) {
+                    subSsum += c - '0';
+                }
+                newS += subSsum;
+                s = newS;
+            }
+            return s;
+        }
+    }
+
+}
