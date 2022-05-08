b0917f0e6ac82c4dd0e26894ed3f1ad3e88a1198
guanxinglu <guanxinglu@futunn.com>
2022-05-08 17:06:00 +0800

    检查是否有合法括号字符串路径

--- offer-algorithm/src/main/java/leetcode/contest/week292/Test6059_检查是否有合法括号字符串路径.java
+++ offer-algorithm/src/main/java/leetcode/contest/week292/Test6059_检查是否有合法括号字符串路径.java
@@ -15,7 +15,52 @@ public class Test6059_检查是否有合法括号字符串路径 {
         public boolean hasValidPath(char[][] grid) {
             int height = grid.length;
             int width = grid[0].length;
+            if (grid[0][0] == ')' || grid[height - 1][width - 1] == '(') {
+                return false;
+            }
+            int[][] dirs = new int[][]{{1, 0}, {0, 1}};
+            boolean[][][] visited = new boolean[height][width][height + width];
             // BFS
+            Queue<int[]> queue = new ArrayDeque<>();
+            queue.add(new int[]{0, 0, grid[0][0] == '(' ? 1 : -1});
+            while (!queue.isEmpty()) {
+                int[] pos = queue.poll();
+                int currentY = pos[0];
+                int currentX = pos[1];
+                int currentStatus = pos[2];
+                if (currentY == height - 1 && currentX == width - 1 && currentStatus == 0) {
+                    return true;
+                }
+
+                // 需要使用dirs数组进行循环遍历，不要手动做条件判断，因为在循环遍历过程中使用了continue，手动的话会跳出外面的循环漏数据
+                for (int[] dir : dirs) {
+                    int nextY = currentY + dir[0];
+                    int nextX = currentX + dir[1];
+                    if (nextY >= height || nextX >= width) {
+                        continue;
+                    }
+                    int nextStatus = currentStatus + (grid[nextY][nextX] == '(' ? 1 : -1);
+                    int[] nextPos = new int[]{nextY, nextX, nextStatus};
+                    if (nextStatus < 0 || visited[nextY][nextX][nextStatus]) {
+                        continue;
+                    }
+                    visited[nextY][nextX][nextStatus] = true;
+                    queue.add(nextPos);
+                }
+            }
+            return false;
+        }
+    }
+
+    static class Solution_BFS记忆化 {
+        public boolean hasValidPath(char[][] grid) {
+            int height = grid.length;
+            int width = grid[0].length;
+            if (grid[0][0] == ')' || grid[height - 1][width - 1] == '(') {
+                return false;
+            }
+            // BFS
+            Set<String> posSet = new HashSet<>();
             Queue<Pos> queue = new ArrayDeque<>();
             queue.add(new Pos(0, 0, grid[0][0] == '(' ? 1 : -1));
             while (!queue.isEmpty()) {
@@ -26,20 +71,21 @@ public class Test6059_检查是否有合法括号字符串路径 {
                 if (currentY == height - 1 && currentX == width - 1 && currentStatus == 0) {
                     return true;
                 }
-                if (currentY + 1 < height) {
-                    int nextStatus = currentStatus + (grid[currentY + 1][currentX] == '(' ? 1 : -1);
-                    Pos nextPos = new Pos(currentY + 1, currentX, nextStatus);
-                    if (nextStatus < 0) {
+
+                // 需要使用dirs数组进行循环遍历，不要手动做条件判断，因为在循环遍历过程中使用了continue，手动的话会跳出外面的循环漏数据
+                int[][] dirs = new int[][]{{1, 0}, {0, 1}};
+                for (int[] dir : dirs) {
+                    int nextY = currentY + dir[0];
+                    int nextX = currentX + dir[1];
+                    if (nextY >= height || nextX >= width) {
                         continue;
                     }
-                    queue.add(nextPos);
-                }
-                if (currentX + 1 < width) {
-                    int nextStatus = currentStatus + (grid[currentY][currentX + 1] == '(' ? 1 : -1);
-                    Pos nextPos = new Pos(currentY, currentX + 1, nextStatus);
-                    if (nextStatus < 0) {
+                    int nextStatus = currentStatus + (grid[nextY][nextX] == '(' ? 1 : -1);
+                    Pos nextPos = new Pos(nextY, nextX, nextStatus);
+                    if (nextStatus < 0 || posSet.contains(nextPos.toString())) {
                         continue;
                     }
+                    posSet.add(nextPos.toString());
                     queue.add(nextPos);
                 }
             }
