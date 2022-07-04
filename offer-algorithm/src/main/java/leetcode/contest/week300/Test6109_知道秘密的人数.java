d8f0b979306706d9f242c650755d90d1d16b0f1d
luguanxing <651571925@qq.com>
2022-07-04 09:45:05 +0800

    知道秘密的人数

--- offer-algorithm/src/main/java/leetcode/contest/week300/Test6109_知道秘密的人数.java
+++ offer-algorithm/src/main/java/leetcode/contest/week300/Test6109_知道秘密的人数.java
@@ -7,16 +7,18 @@ public class Test6109_知道秘密的人数 {
     public static void main(String[] args) {
         System.out.println(new Solution().peopleAwareOfSecret(6, 2, 4));
         System.out.println(new Solution().peopleAwareOfSecret(4, 1, 3));
+        System.out.println(new Solution_DP().peopleAwareOfSecret(6, 2, 4));
+        System.out.println(new Solution_DP().peopleAwareOfSecret(4, 1, 3));
     }
 
     static class Solution {
-        private static int MOD = 1000000007;
+        static int MOD = 1000000007;
 
         public int peopleAwareOfSecret(int n, int delay, int forget) {
             // 使用map存储当天剩余天数和对应数量，模拟每一天的变动
             Map<Integer, Integer> map = new HashMap<>();
             map.put(forget, 1);
-            for (int i = 1; i <= n; i++) {
+            for (int i = 1; i <= n - 1; i++) {
                 Map<Integer, Integer> nextMap = new HashMap<>();
                 long canShareCnt = 0;
                 for (int day : map.keySet()) {
@@ -31,7 +33,7 @@ public class Test6109_知道秘密的人数 {
                         nextMap.put(nextDay, nextCount);
                     }
                 }
-                // 下一天中新增分享的人
+                // 下一天中新增的人
                 if (canShareCnt > 0) {
                     nextMap.put(forget - 1, (int) (canShareCnt % MOD));
                 }
@@ -44,6 +46,31 @@ public class Test6109_知道秘密的人数 {
                 res %= MOD;
                 if (day <= forget - delay) {
                     res += map.get(day);
+                    res %= MOD;
+                }
+            }
+            return res;
+        }
+    }
+
+    static class Solution_DP {
+        public int peopleAwareOfSecret(int n, int delay, int forget) {
+            int MOD = (int) (1e9 + 7);
+            // dp[i]表示第i天新增的人数
+            int[] dp = new int[n + 1];
+            dp[1] = 1;
+            // dp[i]能影响到dp[i+delay]到dp[i+forget]的人数
+            for (int i = 1; i < n; i++) {
+                for (int j = i + delay; j <= Math.min(n, i + forget - 1); j++) {
+                    dp[j] += dp[i];
+                    dp[j] %= MOD;
+                }
+            }
+            // 计算能对第n天产生影响的人数
+            int res = 0;
+            for (int i = 1; i <= n; i++) {
+                if (n < i + forget) {
+                    res += dp[i];
                     res %= MOD;
                 }
             }
