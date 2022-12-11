4cc05a1aa1cce37f38cde8455e9aa73fd50212b5
luguanxing <651571925@qq.com>
2022-12-11 14:54:00 +0800

    矩阵查询可获得的最大分数

--- offer-algorithm/src/main/java/leetcode/contest/week323/Test6260_矩阵查询可获得的最大分数.java
+++ offer-algorithm/src/main/java/leetcode/contest/week323/Test6260_矩阵查询可获得的最大分数.java
@@ -1,23 +1,76 @@
 package leetcode.contest.week323;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class Test6260_矩阵查询可获得的最大分数 {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution().maxPoints(
+        System.out.println(Arrays.toString(new Solution_暴力().maxPoints(
                 new int[][]{{1, 2, 3}, {2, 5, 7}, {3, 5, 1}},
                 new int[]{5, 6, 2}
         )));
-        System.out.println(Arrays.toString(new Solution().maxPoints(
+        System.out.println(Arrays.toString(new Solution_暴力().maxPoints(
                 new int[][]{{5, 2, 1}, {1, 1, 2}},
                 new int[]{3}
         )));
     }
 
-    static class Solution {
+    static class Solution_存储 {
+        int[][] grid;
+        int h;
+        int w;
+
+        public int[] maxPoints(int[][] grid, int[] queries) {
+            this.grid = grid;
+            this.h = grid.length;
+            this.w = grid[0].length;
+            // 把queries排序，这样结果也只会递增
+            Arrays.sort(queries);
+            int[] res = new int[queries.length];
+            for (int i = 0; i < queries.length; i++) {
+                res[i] = query(queries[i]);
+            }
+            return res;
+        }
+
+        int cnt = 0;
+        Set<String> visited = new HashSet<>();
+        Set<String> candidates = new HashSet<>();
+
+        private int query(int k) {
+            // 找出grid里所有小于k且能到达的方块数
+            dfs(0, 0, k);
+            // 如果有之前位置因为k不够大导致无法探索的，从这些位置继续探索
+            for (String candidate : new HashSet<>(candidates)) {
+                int y = Integer.parseInt(candidate.split(",")[0]);
+                int x = Integer.parseInt(candidate.split(",")[1]);
+                dfs(y, x, k);
+            }
+            return cnt;
+        }
+
+        private void dfs(int y, int x, int k) {
+            if (y < 0 || x < 0 || y >= h || x >= w) {
+                return;
+            }
+            if (visited.contains(y + "," + x)) {
+                return;
+            }
+            if (grid[y][x] >= k) {
+                candidates.add(y + "," + x);
+                return;
+            }
+            cnt++;
+            visited.add(y + "," + x);
+            candidates.remove(y + "," + x);
+            dfs(y + 1, x, k);
+            dfs(y - 1, x, k);
+            dfs(y, x + 1, k);
+            dfs(y, x - 1, k);
+        }
+    }
+
+    static class Solution_暴力 {
         int[][] grid;
         int h;
         int w;
