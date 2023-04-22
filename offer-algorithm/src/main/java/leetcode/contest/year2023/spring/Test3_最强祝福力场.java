43afb2ad8dd8ef808b3117b56f69c6d54d754eb4
luguanxing <651571925@qq.com>
2023-04-22 16:01:17 +0800

    最强祝福力场

---
+++ offer-algorithm/src/main/java/leetcode/contest/year2023/spring/Test3_最强祝福力场.java
@@ -0,0 +1,56 @@
+package leetcode.contest.year2023.spring;
+
+import java.util.*;
+
+public class Test3_最强祝福力场 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().fieldOfGreatestBlessing(new int[][]{{0, 0, 1}, {1, 0, 1}}));
+        System.out.println(new Solution().fieldOfGreatestBlessing(new int[][]{{4, 4, 6}, {7, 5, 3}, {1, 6, 2}, {5, 6, 3}}));
+        System.out.println(new Solution().fieldOfGreatestBlessing(new int[][]{{0, 0, 1}, {1, 0, 1}}));
+        System.out.println(new Solution().fieldOfGreatestBlessing(new int[][]{{0, 0, 2}, {4, 0, 2}, {3, 0, 1}}));
+        System.out.println(new Solution().fieldOfGreatestBlessing(new int[][]{{565, 34, 242}, {299, 628, 870}, {724, 673, 221}, {128, 267, 917}, {561, 488, 696}, {341, 71, 604}, {835, 92, 846}, {945, 696, 973}, {554, 776, 430}, {209, 134, 594}, {987, 898, 282}, {819, 154, 462}, {618, 946, 505}, {561, 40, 677}, {602, 863, 657}, {295, 83, 718}, {456, 920, 939}, {94, 717, 813}, {611, 946, 366}, {818, 850, 85}, {395, 554, 333}, {325, 700, 628}, {590, 401, 119}, {248, 858, 499}, {298, 723, 602}, {189, 538, 646}, {194, 283, 344}, {842, 535, 866}, {192, 832, 195}}));
+    }
+
+    static class Solution {
+        public int fieldOfGreatestBlessing(int[][] forceField) {
+            int max = 0;
+            List<double[]> intersections = new ArrayList<>();
+            for (int[] field1 : forceField) {
+                double x1 = field1[0];
+                double y1 = field1[1];
+                double hf1 = field1[2] / 2.0;
+                for (int[] field2 : forceField) {
+                    double x2 = field2[0];
+                    double y2 = field2[1];
+                    double hf2 = field2[2] / 2.0;
+                    double left = Math.max(x1 - hf1, x2 - hf2);
+                    double right = Math.min(x1 + hf1, x2 + hf2);
+                    double top = Math.max(y1 - hf1, y2 - hf2);
+                    double bottom = Math.min(y1 + hf1, y2 + hf2);
+                    if (left <= right && top <= bottom) {
+                        intersections.add(new double[]{left, top});
+                        intersections.add(new double[]{left, bottom});
+                        intersections.add(new double[]{right, top});
+                        intersections.add(new double[]{right, bottom});
+                    }
+                }
+            }
+            for (double[] intersection : intersections) {
+                int current = 0;
+                for (int[] field : forceField) {
+                    double x = field[0];
+                    double y = field[1];
+                    double half = field[2] / 2.0;
+                    if (x - half <= intersection[0] && intersection[0] <= x + half &&
+                            y - half <= intersection[1] && intersection[1] <= y + half) {
+                        current++;
+                    }
+                }
+                max = Math.max(max, current);
+            }
+            return max;
+        }
+    }
+
+}
