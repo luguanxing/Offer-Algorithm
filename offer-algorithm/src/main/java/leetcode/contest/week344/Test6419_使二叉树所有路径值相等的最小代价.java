ef26af4450b7c1051242636899dfbc325b847a89
luguanxing <651571925@qq.com>
2023-05-07 14:26:20 +0800

    使二叉树所有路径值相等的最小代价

---
+++ offer-algorithm/src/main/java/leetcode/contest/week344/Test6419_使二叉树所有路径值相等的最小代价.java
@@ -0,0 +1,69 @@
+package leetcode.contest.week344;
+
+public class Test6419_使二叉树所有路径值相等的最小代价 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().minIncrements(7, new int[]{1, 5, 2, 2, 3, 3, 1}));
+        System.out.println(new Solution().minIncrements(3, new int[]{5, 3, 3}));
+    }
+
+    static class Solution {
+        int ans = 0;
+        int maxPathSum = 0;
+
+        public int minIncrements(int n, int[] cost) {
+            TreeNode root = buildTree(cost, 0);
+            maxPathSum = findMaxPathSum(root);
+            dfs(root, maxPathSum);
+            return ans;
+        }
+
+        private void dfs(TreeNode root, int sum) {
+            if (root == null) {
+                return;
+            }
+            sum -= root.val;
+            int leftNeed = getMaxSum(root.left);
+            int rightNeed = getMaxSum(root.right);
+            int add = sum - Math.max(leftNeed, rightNeed);
+            ans += add;
+            dfs(root.left, sum - add);
+            dfs(root.right, sum - add);
+        }
+
+        private int getMaxSum(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return root.val + Math.max(getMaxSum(root.left), getMaxSum(root.right));
+        }
+
+        public class TreeNode {
+            int val;
+            TreeNode left;
+            TreeNode right;
+
+            TreeNode(int x) {
+                val = x;
+            }
+        }
+
+        private TreeNode buildTree(int[] cost, int index) {
+            if (index >= cost.length) {
+                return null;
+            }
+            TreeNode node = new TreeNode(cost[index]);
+            node.left = buildTree(cost, 2 * index + 1);
+            node.right = buildTree(cost, 2 * index + 2);
+            return node;
+        }
+
+        private int findMaxPathSum(TreeNode root) {
+            if (root == null) {
+                return 0;
+            }
+            return root.val + Math.max(findMaxPathSum(root.left), findMaxPathSum(root.right));
+        }
+    }
+
+}
