c971661e53a7884387bb8521e088a00b8c4f4365
luguanxing <651571925@qq.com>
2023-06-11 12:06:52 +0800

    收集巧克力

---
+++ offer-algorithm/src/main/java/leetcode/contest/week349/Test6449_收集巧克力.java
@@ -0,0 +1,34 @@
+package leetcode.contest.week349;
+
+public class Test6449_收集巧克力 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().minCost(new int[]{20, 1, 15}, 5));
+        System.out.println(new Solution().minCost(new int[]{1, 2, 3}, 4));
+        System.out.println(new Solution().minCost(new int[]{15, 150, 56, 69, 214, 203}, 42));
+    }
+
+    static class Solution {
+        public long minCost(int[] nums, int x) {
+            int len = nums.length;
+            long min = Long.MAX_VALUE;
+            int[] minCost = nums.clone();
+            for (int moveStep = 0; moveStep < len; moveStep++) {
+                if ((long) moveStep * x > min) {
+                    break;
+                }
+                for (int i = 0; i < len; i++) {
+                    minCost[i] = Math.min(minCost[i], nums[(i + moveStep) % len]);
+                }
+                long sum = 0;
+                for (int c : minCost) {
+                    sum += c;
+                }
+                sum += (long) moveStep * x;
+                min = Math.min(min, sum);
+            }
+            return min;
+        }
+    }
+
+}
