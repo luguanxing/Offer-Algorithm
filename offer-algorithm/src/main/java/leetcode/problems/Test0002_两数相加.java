c2ddb6a04f31eb58e42299a9e51b0fc0603281be
luguanxing <651571925@qq.com>
2023-07-02 12:04:17 +0800

    两数相加

--- offer-algorithm/src/main/java/leetcode/problems/Test0002_两数相加.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0002_两数相加.java
@@ -14,13 +14,16 @@ public class Test0002_两数相加 {
     static class ListNode {
         int val;
         ListNode next;
+
         ListNode(int x) {
             val = x;
         }
+
         ListNode(int x, ListNode next) {
             val = x;
             this.next = next;
         }
+
         @Override
         public String toString() {
             return val + "->" + next;
@@ -57,11 +60,40 @@ public class Test0002_两数相加 {
                     list.get(i + 1).val += 1;
                 }
             }
-            if (list.get(list.size()-1).val >= 10) {
-                list.get(list.size()-1).val -= 10;
+            if (list.get(list.size() - 1).val >= 10) {
+                list.get(list.size() - 1).val -= 10;
                 list.add(new ListNode(1));
             }
             // 重新组织链表
+            for (int i = 0; i < list.size() - 1; i++) {
+                list.get(i).next = list.get(i + 1);
+            }
+            return list.get(0);
+        }
+    }
+
+    static class Solution2 {
+        public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+            List<ListNode> list = new ArrayList<>();
+            while (l1 != null || l2 != null) {
+                list.add(new ListNode((l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val)));
+                if (l1 != null) {
+                    l1 = l1.next;
+                }
+                if (l2 != null) {
+                    l2 = l2.next;
+                }
+            }
+            for (int i = 0; i < list.size() - 1; i++) {
+                if (list.get(i).val >= 10) {
+                    list.get(i).val -= 10;
+                    list.get(i + 1).val += 1;
+                }
+            }
+            if (list.get(list.size() - 1).val >= 10) {
+                list.get(list.size() - 1).val -= 10;
+                list.get(list.size() - 1).next = new ListNode(1);
+            }
             for (int i = 0; i < list.size() - 1; i++) {
                 list.get(i).next = list.get(i + 1);
             }
