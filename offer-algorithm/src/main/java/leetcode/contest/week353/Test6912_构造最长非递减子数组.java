0de0f7894d8481a85d6df5a6e73753a5903ca476
luguanxing <651571925@qq.com>
2023-07-09 11:47:40 +0800

    构造最长非递减子数组

--- offer-algorithm/src/main/java/leetcode/contest/week353/Test6912_构造最长非递减子数组.java
+++ offer-algorithm/src/main/java/leetcode/contest/week353/Test6912_构造最长非递减子数组.java
@@ -28,20 +28,27 @@ public class Test6912_构造最长非递减子数组 {
     static class Solution {
         public int maxNonDecreasingLength(int[] nums1, int[] nums2) {
             int n = nums1.length;
-            // dp11表示在i位置选择nums1[i]，在i-1位置也选择了nums1[i-1]
+            // dp11：在nums3的i位置选择nums1[i]，并且在i-1位置也选择了nums1[i-1]时的最大非递减子数组长度
+            // dp12：在nums3的i位置选择nums1[i]，但在i-1位置选择了nums2[i-1]时的最大非递减子数组长度
+            // dp21：在nums3的i位置选择nums2[i]，并且在i-1位置也选择了nums1[i-1]时的最大非递减子数组长度
+            // dp22：在nums3的i位置选择nums2[i]，但在i-1位置选择了nums2[i-1]时的最大非递减子数组长度
             int dp11 = 1, dp21 = 1;
-            int res = 1;
+            int ans = 1;
             for (int i = 1; i < n; ++i) {
+                // 如果nums1[i]大于或等于nums1[i-1]，那么dp11可以增加1（即tmp11=dp11+1），否则nums1[i]单独成为一个新的非递减子数组，所以tmp11=1
+                // 如果nums1[i]大于或等于nums2[i-1]，那么dp12可以增加1（即tmp12=dp21+1），否则nums1[i]单独成为一个新的非递减子数组，所以tmp12=1
+                // 如果nums2[i]大于或等于nums1[i-1]，那么dp21可以增加1（即tmp21=dp11+1），否则nums2[i]单独成为一个新的非递减子数组，所以tmp21=1
+                // 如果nums2[i]大于或等于nums2[i-1]，那么dp22可以增加1（即tmp22=dp21+1），否则nums2[i]单独成为一个新的非递减子数组，所以tmp22=1
                 int t11 = nums1[i] >= nums1[i - 1] ? dp11 + 1 : 1;
                 int t12 = nums1[i] >= nums2[i - 1] ? dp21 + 1 : 1;
                 int t21 = nums2[i] >= nums1[i - 1] ? dp11 + 1 : 1;
                 int t22 = nums2[i] >= nums2[i - 1] ? dp21 + 1 : 1;
                 dp11 = Math.max(t11, t12);
                 dp21 = Math.max(t21, t22);
-                res = Math.max(Math.max(dp11, dp21), res);
+                ans = Math.max(Math.max(dp11, dp21), ans);
             }
-            return res;
+            return ans;
         }
     }
-
 }
+
