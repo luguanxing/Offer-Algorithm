07d7e4e5f0bb002cc3b134427cc1b6d7ae858285
luguanxing <651571925@qq.com>
2023-10-29 10:25:47 +0800

    H指数

--- offer-algorithm/src/main/java/leetcode/problems/Test0274_H指数.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0274_H指数.java
@@ -1,14 +1,34 @@
 package leetcode.problems;
 
+import java.util.Arrays;
+
 public class Test0274_H指数 {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().hIndex(new int[]{0, 1, 3, 5, 6}));
-        System.out.println(new Solution().hIndex(new int[]{0}));
-        System.out.println(new Solution().hIndex(new int[]{100}));
-        System.out.println(new Solution().hIndex(new int[]{1, 1}));
-        System.out.println(new Solution().hIndex(new int[]{1, 2}));
-        System.out.println(new Solution().hIndex(new int[]{11, 15}));
+//        System.out.println(new Solution().hIndex(new int[]{0, 1, 3, 5, 6}));
+//        System.out.println(new Solution().hIndex(new int[]{0}));
+//        System.out.println(new Solution().hIndex(new int[]{100}));
+//        System.out.println(new Solution().hIndex(new int[]{1, 1}));
+//        System.out.println(new Solution().hIndex(new int[]{1, 2}));
+//        System.out.println(new Solution().hIndex(new int[]{11, 15}));
+        System.out.println(new Solution2().hIndex(new int[]{3, 0, 6, 1, 5}));
+        System.out.println(new Solution2().hIndex(new int[]{1, 3, 1}));
+    }
+
+    static class Solution2 {
+        public int hIndex(int[] citations) {
+            Arrays.sort(citations);
+            int len = citations.length;
+            int maxH = 0;
+            for (int h = 1; h <= len; h++) {
+                for (int i = 0; i < len; i++) {
+                    if (citations[i] >= h && len - i >= h) {
+                        maxH = Math.max(maxH, h);
+                    }
+                }
+            }
+            return maxH;
+        }
     }
 
     static class Solution {
