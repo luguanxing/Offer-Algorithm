89a49944181f9fc6ef370a6dc0e65a079c9b6059
luguanxing <651571925@qq.com>
2024-02-28 11:38:22 +0800

    使二叉树所有路径值相等的最小代价

--- offer-algorithm/src/main/java/leetcode/problems/Test2673_使二叉树所有路径值相等的最小代价.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test2673_使二叉树所有路径值相等的最小代价.java
@@ -12,19 +12,20 @@ public class Test2673_使二叉树所有路径值相等的最小代价 {
         int res = 0;
 
         public int minIncrements(int n, int[] cost) {
-            getMax(cost, 1);
+            getPathValue(cost, 1);
             return res;
         }
 
-        private int getMax(int[] cost, int index) {
+        private int getPathValue(int[] cost, int index) {
             if (index > cost.length) {
                 return 0;
             }
-            // 当前节点需要补充的数字，是由左右子树里最大的路径决定的，而不是由左右子树的和
-            int leftMax = getMax(cost, index * 2);
-            int rightMax = getMax(cost, index * 2 + 1);
-            res += Math.abs(leftMax - rightMax);
-            return cost[index - 1] + Math.max(leftMax, rightMax);
+            // 当前节点需要补充的数字，是由左右子树的路径值决定的，而不是由左右子树的和
+            int leftValue = getPathValue(cost, index * 2);
+            int rightValue = getPathValue(cost, index * 2 + 1);
+            res += Math.abs(leftValue - rightValue);
+            // 当前节点路径值 = 当前节点值 + 左右路径值中的最较大值
+            return cost[index - 1] + Math.max(leftValue, rightValue);
         }
     }
 
