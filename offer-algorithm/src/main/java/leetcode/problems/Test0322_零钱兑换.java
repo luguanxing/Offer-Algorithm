7db6fef513d1c18a1a907ee26f6278806a5a94cc
luguanxing <651571925@qq.com>
2024-03-24 12:21:51 +0800

    零钱兑换

--- offer-algorithm/src/main/java/leetcode/problems/Test0322_零钱兑换.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0322_零钱兑换.java
@@ -10,10 +10,32 @@ public class Test0322_零钱兑换 {
     public static void main(String[] args) {
         System.out.println(new Solution().coinChange(new int[]{1, 2, 5}, 11));
         System.out.println(new Solution().coinChange(new int[]{2}, 3));
+        System.out.println(new Solution().coinChange(new int[]{1}, 0));
         System.out.println(new Solution().coinChange(new int[]{186, 419, 83, 408}, 6249));
     }
 
     static class Solution {
+        public int coinChange(int[] coins, int amount) {
+            Arrays.sort(coins);
+            int[] dp = new int[amount + 1];
+            for (int coin : coins) {
+                if (coin <= amount) {
+                    dp[coin] = 1;
+                }
+            }
+            for (int i = 1; i <= amount; i++) {
+                dp[i] = Integer.MAX_VALUE / 2;
+                for (int coin : coins) {
+                    if (i - coin >= 0) {
+                        dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                    }
+                }
+            }
+            return dp[amount] > Integer.MAX_VALUE / 2 ? -1 : dp[amount];
+        }
+    }
+
+    static class Solution_DP {
         public int coinChange(int[] coins, int amount) {
             // dp[i]表示凑出i元使用的最少硬币数量
             int[] dp = new int[amount + 1];
