f6d71ef76277bf37be2cd589c78bef736f856199
luguanxing <651571925@qq.com>
2024-04-20 10:27:41 +0800

    组合总和

--- offer-algorithm/src/main/java/leetcode/problems/Test0039_组合总和.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0039_组合总和.java
@@ -8,9 +8,39 @@ public class Test0039_组合总和 {
     public static void main(String[] args) {
         System.out.println(new Solution().combinationSum(new int[]{2, 3, 6, 7}, 7));
         System.out.println(new Solution().combinationSum(new int[]{2, 3, 5}, 8));
+        System.out.println(new Solution().combinationSum(new int[]{2}, 1));
     }
 
     static class Solution {
+        private List<List<Integer>> result = new ArrayList<>();
+
+        public List<List<Integer>> combinationSum(int[] candidates, int target) {
+            Arrays.sort(candidates);
+            dfs(candidates, target, 0, new ArrayList<>());
+            return result;
+        }
+
+        private void dfs(int[] candidates, int target, int index, List<Integer> current) {
+            if (target == 0) {
+                result.add(new ArrayList<>(current));
+                return;
+            }
+            if (index >= candidates.length) {
+                return;
+            }
+            if (candidates[index] > target) {
+                return;
+            }
+            // 选择要index
+            current.add(candidates[index]);
+            dfs(candidates, target - candidates[index], index, current);
+            current.remove(current.size() - 1);
+            // 选择不要index
+            dfs(candidates, target, index + 1, current);
+        }
+    }
+
+    static class Solution_OLD {
         private List<List<Integer>> result = new ArrayList<>();
         private List<Integer> current = new ArrayList<>();
 
