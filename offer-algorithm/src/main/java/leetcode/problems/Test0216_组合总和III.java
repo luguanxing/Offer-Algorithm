52bd3d898c535b9535126c1ffa0a2bcfefe4740c
luguanxing <651571925@qq.com>
2024-04-21 10:13:53 +0800

    组合总和 III

--- offer-algorithm/src/main/java/leetcode/problems/Test0216_组合总和III.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0216_组合总和III.java
@@ -8,9 +8,36 @@ public class Test0216_组合总和III {
     public static void main(String[] args) {
         System.out.println(new Solution().combinationSum3(3, 7));
         System.out.println(new Solution().combinationSum3(3, 9));
+        System.out.println(new Solution().combinationSum3(4, 1));
+        System.out.println(new Solution().combinationSum3(1, 3));
     }
 
     static class Solution {
+        private List<List<Integer>> result = new ArrayList<>();
+
+        public List<List<Integer>> combinationSum3(int k, int n) {
+            dfs(k, n, 1, new ArrayList<>());
+            return result;
+        }
+
+        private void dfs(int leftCnt, int target, int index, List<Integer> currents) {
+            if (leftCnt == 0 && target == 0) {
+                result.add(new ArrayList<>(currents));
+                return;
+            }
+            if (index > target || index > 9 || leftCnt == 0) {
+                return;
+            }
+            // 选择当前数
+            currents.add(index);
+            dfs(leftCnt - 1, target - index, index + 1, currents);
+            currents.remove(currents.size() - 1);
+            // 不选当前数
+            dfs(leftCnt, target, index + 1, currents);
+        }
+    }
+
+    static class Solution_OLD {
         private List<List<Integer>> result = new ArrayList<>();
         private List<Integer> current = new ArrayList<>();
 
