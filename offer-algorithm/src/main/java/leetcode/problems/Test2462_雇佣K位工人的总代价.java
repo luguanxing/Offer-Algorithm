369262aff366cd817de66b1468533d703ed0dff3
luguanxing <651571925@qq.com>
2024-05-01 11:50:34 +0800

    雇佣K位工人的总代价

--- offer-algorithm/src/main/java/leetcode/problems/Test2462_雇佣K位工人的总代价.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test2462_雇佣K位工人的总代价.java
@@ -14,6 +14,59 @@ public class Test2462_雇佣K位工人的总代价 {
 
     static class Solution {
         public long totalCost(int[] costs, int k, int candidates) {
+            // 两头treemap实现
+            List<Integer> list = Arrays.stream(costs).boxed().collect(Collectors.toList());
+            TreeMap<Integer, Integer> left = new TreeMap<>();
+            TreeMap<Integer, Integer> right = new TreeMap<>();
+            for (int i = 0; i < candidates; i++) {
+                if (list.isEmpty()) {
+                    break;
+                }
+                left.put(list.get(0), left.getOrDefault(list.get(0), 0) + 1);
+                list.remove(0);
+            }
+            for (int i = 0; i < candidates; i++) {
+                if (list.isEmpty()) {
+                    break;
+                }
+                right.put(list.get(list.size() - 1), right.getOrDefault(list.get(list.size() - 1), 0) + 1);
+                list.remove(list.size() - 1);
+            }
+            // 拿取k个，每次从左右找最小，拿取后若还有剩余需要补充
+            long totalCost = 0;
+            for (int i = 1; i <= k; i++) {
+                if (right.isEmpty() || (!left.isEmpty() && left.firstKey() <= right.firstKey())) {
+                    int take = left.firstKey();
+                    totalCost += take;
+                    if (left.get(take) == 1) {
+                        left.remove(take);
+                    } else {
+                        left.put(take, left.get(take) - 1);
+                    }
+                    if (!list.isEmpty()) {
+                        left.put(list.get(0), left.getOrDefault(list.get(0), 0) + 1);
+                        list.remove(0);
+                    }
+                } else if (left.isEmpty() || (!right.isEmpty() && left.firstKey() > right.firstKey())) {
+                    int take = right.firstKey();
+                    totalCost += take;
+                    if (right.get(take) == 1) {
+                        right.remove(take);
+                    } else {
+                        right.put(take, right.get(take) - 1);
+                    }
+                    if (!list.isEmpty()) {
+                        right.put(list.get(list.size() - 1), right.getOrDefault(list.get(list.size() - 1), 0) + 1);
+                        list.remove(list.size() - 1);
+                    }
+                }
+            }
+            return totalCost;
+        }
+    }
+
+    static class Solution_PQ {
+        public long totalCost(int[] costs, int k, int candidates) {
             // 两头优先队列实现
             List<Integer> list = Arrays.stream(costs).boxed().collect(Collectors.toList());
             PriorityQueue<Integer> left = new PriorityQueue<>(Comparator.naturalOrder());
