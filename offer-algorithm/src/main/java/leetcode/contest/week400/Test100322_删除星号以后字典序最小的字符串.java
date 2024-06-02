5db7f2dea5d91fe32112a87a2417665e2a2f4401
luguanxing <651571925@qq.com>
2024-06-02 11:15:42 +0800

    删除星号以后字典序最小的字符串

--- offer-algorithm/src/main/java/leetcode/contest/week400/Test100322_删除星号以后字典序最小的字符串.java
+++ offer-algorithm/src/main/java/leetcode/contest/week400/Test100322_删除星号以后字典序最小的字符串.java
@@ -1,37 +1,48 @@
 package leetcode.contest.week400;
 
-import java.util.TreeMap;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Test100322_删除星号以后字典序最小的字符串 {
 
     public static void main(String[] args) {
         System.out.println(new Solution().clearStars("aaba*"));
         System.out.println(new Solution().clearStars("abc"));
+        System.out.println(new Solution().clearStars("d*yed"));
+        System.out.println(new Solution().clearStars("d*ded*"));
+        System.out.println(new Solution().clearStars("d*dad*"));
     }
 
     static class Solution {
         public String clearStars(String s) {
-            // 构建Map和子序列
-            TreeMap<Character, Integer> map = new TreeMap<>();
-            StringBuilder result = new StringBuilder();
-            for (char c : s.toCharArray()) {
+            // 统计字符频率和位置
+            TreeMap<Character, Integer> charCntMap = new TreeMap<>();
+            Map<Character, List<Integer>> charIndexMap = new HashMap<>();
+            // 存储字符结果
+            List<Character> resultList = new ArrayList<>();
+            for (int i = 0; i < s.length(); i++) {
+                char c = s.charAt(i);
                 if (c != '*') {
-                    map.put(c, map.getOrDefault(c, 0) + 1);
-                    result.append(c);
+                    charCntMap.put(c, charCntMap.getOrDefault(c, 0) + 1);
+                    resultList.add(c);
+                    charIndexMap.putIfAbsent(c, new LinkedList<>());
+                    charIndexMap.get(c).add(resultList.size() - 1);
                 } else {
-                    if (!map.isEmpty()) {
-                        char minChar = map.firstKey();
-                        if (map.get(minChar) == 1) {
-                            map.remove(minChar);
+                    if (!charCntMap.isEmpty()) {
+                        char minChar = charCntMap.firstKey();
+                        if (charCntMap.get(minChar) == 1) {
+                            charCntMap.remove(minChar);
                         } else {
-                            map.put(minChar, map.get(minChar) - 1);
+                            charCntMap.put(minChar, charCntMap.get(minChar) - 1);
                         }
-                        int index = result.lastIndexOf(String.valueOf(minChar));
-                        result.deleteCharAt(index);
+                        List<Integer> positions = charIndexMap.get(minChar);
+                        int pos = positions.remove(positions.size() - 1);
+                        resultList.set(pos, null);
                     }
                 }
             }
-            return result.toString();
+            // 拼接非null的答案并返回
+            return resultList.stream().filter(Objects::nonNull) .map(String::valueOf).collect(Collectors.joining());
         }
     }
 }
