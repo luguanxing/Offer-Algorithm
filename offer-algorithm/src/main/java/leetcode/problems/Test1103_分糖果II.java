c372b371703c322bddbeac5912a1274ef4199577
luguanxing <651571925@qq.com>
2024-06-03 14:39:26 +0800

    分糖果 II

---
+++ offer-algorithm/src/main/java/leetcode/problems/Test1103_分糖果II.java
@@ -0,0 +1,32 @@
+package leetcode.problems;
+
+import java.util.Arrays;
+
+public class Test1103_分糖果II {
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().distributeCandies(7, 4)));
+        System.out.println(Arrays.toString(new Solution().distributeCandies(10, 3)));
+    }
+
+    static class Solution {
+        public int[] distributeCandies(int candies, int num_people) {
+            int index = 1;
+            int[] res = new int[num_people];
+            while (candies > 0) {
+                if (index < candies) {
+                    // 可放入足够的糖
+                    res[(index - 1) % num_people] += index;
+                    candies -= index;
+                    index++;
+                } else {
+                    // 最后一人拿完剩下的糖
+                    res[(index - 1) % num_people] += candies;
+                    candies = 0;
+                }
+            }
+            return res;
+        }
+    }
+
+}
