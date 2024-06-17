579e682a9af126103879f9991554063bbac9cd6c
luguanxing <651571925@qq.com>
2024-06-17 10:19:18 +0800

    最长特殊序列 II

--- offer-algorithm/src/main/java/leetcode/problems/Test0522_最长特殊序列II.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0522_最长特殊序列II.java
@@ -3,11 +3,48 @@ package leetcode.problems;
 public class Test0522_最长特殊序列II {
 
     public static void main(String[] args) {
-        System.out.println(new Solution().findLUSlength(new String[]{"aba","cdc","eae"}));
-        System.out.println(new Solution().findLUSlength(new String[]{"aaa","aaa","aa"}));
+        System.out.println(new Solution().findLUSlength(new String[]{"aba", "cdc", "eae"}));
+        System.out.println(new Solution().findLUSlength(new String[]{"aaa", "aaa", "aa"}));
     }
 
     static class Solution {
+        public int findLUSlength(String[] strs) {
+            int len = strs.length;
+            int max = -1;
+            for (int i = 0; i < len; i++) {
+                String currentStr = strs[i];
+                boolean isIndependent = true;
+                for (int j = 0; j < len; j++) {
+                    String otherStr = strs[j];
+                    if (j == i || currentStr.length() > otherStr.length()) {
+                        continue;
+                    }
+                    if (isSubSeq(currentStr, otherStr)) {
+                        isIndependent = false;
+                        break;
+                    }
+                }
+                if (isIndependent) {
+                    max = Math.max(max, currentStr.length());
+                }
+            }
+            return max;
+        }
+
+        private boolean isSubSeq(String currentStr, String otherStr) {
+            int subIndex = 0;
+            int otherIndex = 0;
+            while (otherIndex < otherStr.length() && subIndex < currentStr.length()) {
+                if (currentStr.charAt(subIndex) == otherStr.charAt(otherIndex)) {
+                    subIndex++;
+                }
+                otherIndex++;
+            }
+            return subIndex == currentStr.length();
+        }
+    }
+
+    static class Solution_OLD {
         public int findLUSlength(String[] strs) {
             int len = strs.length;
             int max = -1;
