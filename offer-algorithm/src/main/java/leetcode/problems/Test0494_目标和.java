7e9b044bd117d46ac532f724d75ef0b8fd75991d
luguanxing <651571925@qq.com>
2024-06-30 01:46:22 +0800

    目标和

--- offer-algorithm/src/main/java/leetcode/problems/Test0494_目标和.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0494_目标和.java
@@ -1,6 +1,8 @@
 package leetcode.problems;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Test0494_目标和 {
 
@@ -14,22 +16,43 @@ public class Test0494_目标和 {
     }
 
     static class Solution {
+        Map<String, Integer> memo = new HashMap<>();
+
+        public int findTargetSumWays(int[] nums, int target) {
+            return dfs(nums, nums.length - 1, target);
+        }
+
+        private int dfs(int[] nums, int endIndex, int target) {
+            if (endIndex == -1) {
+                return target == 0 ? 1 : 0;
+            }
+            if (memo.containsKey(endIndex + "-" + target)) {
+                return memo.get(endIndex + "-" + target);
+            }
+            int num = nums[endIndex];
+            int res = 0;
+            res += dfs(nums, endIndex - 1, target - num);
+            res += dfs(nums, endIndex - 1, target + num);
+            memo.put(endIndex + "-" + target, res);
+            return res;
+        }
+    }
+
+    class Solution_DP {
         public int findTargetSumWays(int[] nums, int target) {
             // sum(子集1) - sum(子集2) = target
             // sum(子集1) - (sum(全集) - sum(子集1)) = target
             // 2 * sum(子集1) = target + sum(全集)
             // 求出 sum(子集1) = (target + sum(全集)) / 2 方法数
             int sum = Arrays.stream(nums).sum();
-            if (sum < target || (target + sum) % 2 == 1) {
+            if (sum < target || (target + sum) % 2 == 1 || target + sum < 0) {
                 return 0;
             }
             int[] dp = new int[(target + sum) / 2 + 1];
             dp[0] = 1;
-            for (int i = (target + sum) / 2; i >= 1; i--) {
-                for (int num : nums) {
-                    if (i >= num) {
-                        dp[i] += dp[i - num];
-                    }
+            for (int num : nums) {
+                for (int i = (target + sum) / 2; i >= num; i--) {
+                    dp[i] += dp[i - num];
                 }
             }
             return dp[(target + sum) / 2];
