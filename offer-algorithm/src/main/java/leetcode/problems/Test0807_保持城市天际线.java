b3c3e34c7bd79dacae97180070eed3c0084dd409
luguanxing <651571925@qq.com>
2024-07-14 11:34:36 +0800

    保持城市天际线

--- offer-algorithm/src/main/java/leetcode/problems/Test0807_保持城市天际线.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0807_保持城市天际线.java
@@ -1,5 +1,7 @@
 package leetcode.problems;
 
+import java.util.*;
+
 public class Test0807_保持城市天际线 {
 
     public static void main(String[] args) {
@@ -9,6 +11,36 @@ public class Test0807_保持城市天际线 {
     }
 
     static class Solution {
+        public int maxIncreaseKeepingSkyline(int[][] grid) {
+            int height = grid.length;
+            int width = grid[0].length;
+            int res = 0;
+            // 计算城市横向和纵向天际线
+            int[] rowMaxs = new int[height];
+            int[] colMaxs = new int[width];
+            for (int y = 0; y < height; y++) {
+                int[] row = grid[y];
+                int rowMax = Arrays.stream(row).max().getAsInt();
+                rowMaxs[y] = rowMax;
+            }
+            for (int x = 0; x < width; x++) {
+                int colMax = 0;
+                for (int y = 0; y < height; y++) {
+                    colMax = Math.max(colMax, grid[y][x]);
+                }
+                colMaxs[x] = colMax;
+            }
+            // 计算增量和
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    res += Math.min(rowMaxs[y], colMaxs[x]) - grid[y][x];
+                }
+            }
+            return res;
+        }
+    }
+
+    static class Solution_OLD {
         public int maxIncreaseKeepingSkyline(int[][] grid) {
             int height = grid.length;
             int width = grid[0].length;
