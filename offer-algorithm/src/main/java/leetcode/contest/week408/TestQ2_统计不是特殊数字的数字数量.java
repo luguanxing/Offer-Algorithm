6a4fadc1191917748b3dca24a9f62b8654d03169
luguanxing <651571925@qq.com>
2024-07-28 10:44:16 +0800

    统计不是特殊数字的数字数量

---
+++ offer-algorithm/src/main/java/leetcode/contest/week408/TestQ2_统计不是特殊数字的数字数量.java
@@ -0,0 +1,39 @@
+package leetcode.contest.week408;
+
+import java.util.*;
+
+public class TestQ2_统计不是特殊数字的数字数量 {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().nonSpecialCount(5, 7));
+        System.out.println(new Solution().nonSpecialCount(4, 16));
+    }
+
+    static class Solution {
+        public int nonSpecialCount(int l, int r) {
+            TreeSet<Integer> squares = new TreeSet<>();
+            int start = (int) Math.ceil(Math.sqrt(l));
+            int end = (int) Math.floor(Math.sqrt(r));
+            for (int i = start; i <= end; i++) {
+                // 只有质数的平方才是特殊数字
+                if (isPrime(i)) {
+                    squares.add(i * i);
+                }
+            }
+            return r - l + 1 - squares.size();
+        }
+
+        private boolean isPrime(int n) {
+            if (n == 1) {
+                return false;
+            }
+            for (int i = 2; i * i <= n; i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+}
