ae97a03a23335136005d7f952556827ebd981e3d
luguanxing <651571925@qq.com>
2024-09-23 09:18:16 +0800

    最佳观光组合

--- offer-algorithm/src/main/java/leetcode/problems/Test1014_最佳观光组合.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test1014_最佳观光组合.java
@@ -1,12 +1,29 @@
 package leetcode.problems;
 
+import java.util.*;
+
 public class Test1014_最佳观光组合 {
 
     public static void main(String[] args) {
         System.out.println(new Solution().maxScoreSightseeingPair(new int[]{8, 1, 5, 2, 6}));
+        System.out.println(new Solution().maxScoreSightseeingPair(new int[]{1, 2}));
     }
 
     static class Solution {
+        public int maxScoreSightseeingPair(int[] values) {
+            // 枚举j，记录最大的values[j]-j + 前面保存最大的(values[i] + i)
+            int max = 0;
+            int last = values[0] + 0;
+            for (int j = 1; j < values.length; j++) {
+                int current = values[j] - j;
+                max = Math.max(max, current + last);
+                last = Math.max(last, values[j] + j);
+            }
+            return max;
+        }
+    }
+
+    static class Solution_OLD {
         public int maxScoreSightseeingPair(int[] A) {
             // 保留上个最佳观光点的状态
             int lastPoint = A[0];
