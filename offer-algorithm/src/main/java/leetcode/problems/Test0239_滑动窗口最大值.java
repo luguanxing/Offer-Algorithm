a3814389fc9ba4fdd921e36db2ce4f353a91dd80
luguanxing <651571925@qq.com>
2024-11-12 11:14:47 +0800

    滑动窗口最大值

--- offer-algorithm/src/main/java/leetcode/problems/Test0239_滑动窗口最大值.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test0239_滑动窗口最大值.java
@@ -5,24 +5,121 @@ import java.util.*;
 public class Test0239_滑动窗口最大值 {
 
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
+        System.out.println(Arrays.toString(new Solution().maxSlidingWindow(
                 new int[]{1, 3, -1, -3, 5, 3, 6, 7}, 3
         )));
-        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
-                new int[]{1}, 1
-        )));
-        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
-                new int[]{1, -1}, 1
-        )));
-        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
-                new int[]{9, 11}, 2
-        )));
-        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
-                new int[]{4, -2}, 2
-        )));
-        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
-                new int[]{1, 3, 1, 2, 0, 5}, 3
-        )));
+//        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
+//                new int[]{1}, 1
+//        )));
+//        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
+//                new int[]{1, -1}, 1
+//        )));
+//        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
+//                new int[]{9, 11}, 2
+//        )));
+//        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
+//                new int[]{4, -2}, 2
+//        )));
+//        System.out.println(Arrays.toString(new Solution_单调栈优化().maxSlidingWindow(
+//                new int[]{1, 3, 1, 2, 0, 5}, 3
+//        )));
+    }
+
+    static class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            List<Integer> resList = new ArrayList<>();
+            MyDeque myDeque = new MyDeque();
+            for (int i = 0; i < nums.length; i++) {
+                myDeque.push(nums[i]);
+                if (i >= k - 1) {
+                    // 取数，同时比较单调队列左边和窗口左侧
+                    resList.add(myDeque.poll(nums[i-k+1]));
+                }
+            }
+            // 返回结果
+            int[] res = new int[resList.size()];
+            for (int i = 0; i < resList.size(); i++) {
+                res[i] = resList.get(i);
+            }
+            return res;
+        }
+
+        class MyDeque {
+            Deque<Integer> deque;
+
+            public MyDeque() {
+                this.deque = new ArrayDeque<>();
+            }
+
+            public void push(int num) {
+                // 单调栈：保证内递减
+                while (!deque.isEmpty()) {
+                    if (deque.peekLast() < num) {
+                        deque.pollLast();
+                    } else {
+                        break;
+                    }
+                }
+                deque.addLast(num);
+            }
+
+            public int poll(int num) {
+                // 只有单调队列左边和窗口左边对上时进行移除
+                int head = deque.peekFirst();
+                if (head == num) {
+                    deque.pollFirst();
+                }
+                return head;
+            }
+        }
+    }
+
+    static class Solution_未优化 {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            List<Integer> resList = new ArrayList<>();
+            MyDeque myDeque = new MyDeque();
+            for (int i = 0; i < nums.length; i++) {
+                myDeque.push(nums[i]);
+                if (i >= k - 1) {
+                    resList.add(myDeque.poll());
+                }
+            }
+            // 返回结果
+            int[] res = new int[resList.size()];
+            for (int i = 0; i < resList.size(); i++) {
+                res[i] = resList.get(i);
+            }
+            return res;
+        }
+
+        class MyDeque {
+            Deque<Integer> deque;
+
+            public MyDeque() {
+                this.deque = new ArrayDeque<>();
+            }
+
+            public void push(int num) {
+                // 单调栈：保证内递减
+                int smallerCnt = 0;
+                while (!deque.isEmpty()) {
+                    if (deque.peekLast() < num) {
+                        deque.pollLast();
+                        smallerCnt++;
+                    } else {
+                        break;
+                    }
+                }
+                deque.addLast(num);
+                for (int i = 1; i <= smallerCnt; i++) {
+                    deque.addLast(num);
+                }
+            }
+
+            public int poll() {
+                return deque.pollFirst();
+            }
+        }
     }
 
     static class Solution_单调栈优化 {
@@ -124,7 +221,7 @@ public class Test0239_滑动窗口最大值 {
         }
     }
 
-    static class Solution {
+    static class Solution_OLD {
         public int[] maxSlidingWindow(int[] nums, int k) {
             int[] res = new int[nums.length - k + 1];
             // 窗口内保持递减，遇到更大的替换前面全部
