5e2c7974846e42337183ecef71d2574225a40d2b
luguanxing <651571925@qq.com>
2024-12-29 11:45:16 +0800

    统计恰好有K个相等相邻元素的数组数目

--- offer-algorithm/src/main/java/leetcode/contest/week430/Test100507_统计恰好有K个相等相邻元素的数组数目.java
+++ offer-algorithm/src/main/java/leetcode/contest/week430/Test100507_统计恰好有K个相等相邻元素的数组数目.java
@@ -9,13 +9,13 @@ public class Test100507_统计恰好有K个相等相邻元素的数组数目 {
     }
 
     static class Solution {
-        private int MOD = (int) (1E9 + 7);
-        private int MAX = (int) (1E5 + 1);
-        private long[] factorial = new long[MAX];
-        private long[] invFactorial = new long[MAX];
+        int MOD = (int) (1E9 + 7);
+        int MAX = (int) (1E5 + 1);
+        long[] factorial = new long[MAX];
+        long[] invFactorial = new long[MAX];
 
         public int countGoodArrays(int n, int m, int k) {
-            // 初始化
+            // 初始化阶乘和逆阶乘
             factorial[0] = 1;
             for (int i = 1; i < MAX; i++) {
                 factorial[i] = factorial[i - 1] * i % MOD;
@@ -24,15 +24,16 @@ public class Test100507_统计恰好有K个相等相邻元素的数组数目 {
             for (int i = MAX - 2; i >= 0; i--) {
                 invFactorial[i] = invFactorial[i + 1] * (i + 1) % MOD;
             }
-            // 计算组合数
-            if (k > n - 1) {
-                return 0;
-            }
+            // 计算组合数，有C(n-1, k) 种组合
             long comb = comb(n - 1, k);
-            // 快速幂
+            // 快速幂，相同位置有m种选择，不同位置有(m-1)种
             long power = pow(m - 1, n - 1 - k);
-            long res = ((long) m * comb) % MOD;
-            res = (res * power) % MOD;
+            // res = m * C(n-1, k) * (m-1)^(n-1-k)
+            long res = m;
+            res *= comb;
+            res %= MOD;
+            res *= power;
+            res %= MOD;
             return (int) res;
         }
 
