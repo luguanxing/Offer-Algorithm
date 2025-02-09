06a6b92812149312542686e3cab9080faeaa89a5
luguanxing <651571925@qq.com>
2025-02-09 10:57:14 +0800

    将元素分配给有约束条件的组

--- offer-algorithm/src/main/java/leetcode/contest/week436/Test100572_将元素分配给有约束条件的组.java
+++ offer-algorithm/src/main/java/leetcode/contest/week436/Test100572_将元素分配给有约束条件的组.java
@@ -16,42 +16,42 @@ public class Test100572_将元素分配给有约束条件的组 {
     static class Solution {
         public int[] assignElements(int[] groups, int[] elements) {
             int eLen = elements.length;
+            int gLen = groups.length;
             int MAX = (int) (1E5+5);
 
             // 记录每个数值的最小下标
-            int[] minIndex = new int[MAX];
-            Arrays.fill(minIndex, -1);
+            int[] minElementIndex = new int[MAX];
+            Arrays.fill(minElementIndex, -1);
             for (int i = 0; i < eLen; i++) {
                 int val = elements[i];
-                if (minIndex[val] == -1) {
-                    minIndex[val] = i;
+                if (minElementIndex[val] == -1) {
+                    minElementIndex[val] = i;
                 }
             }
 
             // 对每个组，找出满足 groups[i] % elements[j] == 0 的 j，取下标最小的那个
-            int gLen = groups.length;
-            int[] res = new int[gLen];
+            int[] minDivIndex = new int[gLen];
             for (int i = 0; i < gLen; i++) {
                 int groupVal = groups[i];
-                int candidateIndex = Integer.MAX_VALUE;
+                int divIndex = Integer.MAX_VALUE;
                 int sqrt = (int) Math.sqrt(groupVal);
                 for (int d = 1; d <= sqrt; d++) {
                     if (groupVal % d == 0) {
                         int other = groupVal / d;
                         // 检查第一个因子 d
-                        if (minIndex[d] != -1) {
-                            candidateIndex = Math.min(candidateIndex, minIndex[d]);
+                        if (minElementIndex[d] != -1) {
+                            divIndex = Math.min(divIndex, minElementIndex[d]);
                         }
                         // 检查另一因子 other
-                        if (other != d && minIndex[other] != -1) {
-                            candidateIndex = Math.min(candidateIndex, minIndex[other]);
+                        if (other != d && minElementIndex[other] != -1) {
+                            divIndex = Math.min(divIndex, minElementIndex[other]);
                         }
                     }
                 }
-                res[i] = (candidateIndex == Integer.MAX_VALUE) ? -1 : candidateIndex;
+                minDivIndex[i] = (divIndex == Integer.MAX_VALUE) ? -1 : divIndex;
             }
 
-            return res;
+            return minDivIndex;
         }
     }
 
