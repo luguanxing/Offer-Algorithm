ca16e750cbc0bf78b0c9f414152dfbf3c9e349a9
luguanxing <651571925@qq.com>
2025-02-12 10:45:16 +0800

    袋子里最少数目的球

--- offer-algorithm/src/main/java/leetcode/problems/Test1760_袋子里最少数目的球.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test1760_袋子里最少数目的球.java
@@ -12,6 +12,30 @@ public class Test1760_袋子里最少数目的球 {
 
     static class Solution {
         public int minimumSize(int[] nums, int maxOperations) {
+            // 使用二分不断尝试能满足操作数小于maxOperations的容量
+            int l = 1;
+            int r = Arrays.stream(nums).max().getAsInt();
+            while (l <= r) {
+                int currentSize = (l + r) / 2;
+                // 计算按照当前容量需要的操作数
+                int currentOperations = 0;
+                for (int num : nums) {
+                    currentOperations += (num - 1) / currentSize;
+                }
+                // 如果操作数超过了maxOperations，说明容量太小，尝试增大容量
+                // 如果操作数小于等于maxOperations，说明容量足够，尝试减小容量
+                if (currentOperations > maxOperations) {
+                    l = currentSize + 1;
+                } else {
+                    r = currentSize - 1;
+                }
+            }
+            return l;
+        }
+    }
+
+    static class Solution_OLD {
+        public int minimumSize(int[] nums, int maxOperations) {
             // 使用二分不断试探出判断袋子球的最多数量的最小值
             int left = 1;
             int right = Arrays.stream(nums).max().getAsInt();
