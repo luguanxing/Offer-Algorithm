4675072b9179e6428e6b98ab81160ee768c5cf24
luguanxing <651571925@qq.com>
2025-03-12 14:57:05 +0800

    元音辅音字符串计数I

---
+++ offer-algorithm/src/main/java/leetcode/problems/Test3305_元音辅音字符串计数I.java
@@ -0,0 +1,37 @@
+package leetcode.problems;
+
+import java.util.*;
+
+public class Test3305_元音辅音字符串计数I {
+
+    public static void main(String[] args) {
+        System.out.println(new Solution().countOfSubstrings("aeioqq", 1));
+    }
+
+    static class Solution {
+        public int countOfSubstrings(String word, int k) {
+            int res = 0;
+            for (int i = 0; i < word.length(); i++) {
+                for (int j = i+1; j <= word.length(); j++) {
+                    String str = word.substring(i,j);
+                    if (ok(str, k)) {
+                        System.out.println(str);
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+
+        public boolean ok(String str, int k) {
+            Map<Character, Integer> map = new HashMap<>();
+            for (char c : str.toCharArray()) {
+                if (c != 'a' && c != 'e' && c != 'i' && c != 'o' && c != 'u') {
+                    map.put(c, map.getOrDefault(c, 0) + 1);
+                }
+            }
+            return map.values().stream().reduce(Integer::sum).orElse(0)==k && str.contains("a") && str.contains("e") && str.contains("i") && str.contains("o") && str.contains("u");
+        }
+    }
+
+}
