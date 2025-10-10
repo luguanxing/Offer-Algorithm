fc45fe3ff112ee7c0bbb39a1bfff42e5149b9a39
luguanxing <651571925@qq.com>
2025-10-10 15:39:13 +0800

    从魔法师身上吸取的最大能量

---
+++ offer-algorithm/src/main/java/leetcode/problems/Test3147_从魔法师身上吸取的最大能量.java
@@ -0,0 +1,26 @@
+package leetcode.problems;
+
+import java.util.Arrays;
+
+public class Test3147_从魔法师身上吸取的最大能量 {
+
+    public static void main(String[] args) {
+        // energy = [5,2,-10,-5,1], k = 3
+        System.out.println(new Solution().maximumEnergy(new int[]{5, 2, -10, -5, 1}, 3));
+        // energy = [-2,-3,-1], k = 2
+        System.out.println(new Solution().maximumEnergy(new int[]{-2, -3, -1}, 2));
+    }
+
+    static class Solution {
+        public int maximumEnergy(int[] energy, int k) {
+            int len = energy.length;
+            // 维持一个k的窗口
+            int[] window = new int[k];
+            for (int i = 0; i < len; i++) {
+                window[i % k] = Math.max(window[i % k] + energy[i], energy[i]);
+            }
+            return Arrays.stream(window).max().getAsInt();
+        }
+    }
+
+}
