89cf39107bb68be961b84625f16a6478e135b26c
luguanxing <651571925@qq.com>
2026-04-17 10:22:21 +0800

    镜像对之间最小绝对距离

--- offer-algorithm/src/main/java/leetcode/problems/Test3761_镜像对之间最小绝对距离.java
+++ offer-algorithm/src/main/java/leetcode/problems/Test3761_镜像对之间最小绝对距离.java
@@ -17,17 +17,23 @@ public class Test3761_镜像对之间最小绝对距离 {
             Map<Integer, Integer> reIdxMap = new HashMap<>();
             for (int i = 0; i < nums.length; i++) {
                 int num = nums[i];
-                int reNum = Integer.parseInt(
-                        new StringBuilder(String.valueOf(num))
-                                .reverse()
-                                .toString()
-                );
+                int reNum = reverseNum(num);
                 if (reIdxMap.containsKey(num)) {
                     res = Math.min(res, i - reIdxMap.get(num));
                 }
                 reIdxMap.put(reNum, i);
             }
             return res == Integer.MAX_VALUE ? -1 : res;
+        }
+
+        private int reverseNum(int num) {
+            int r = 0;
+            while (num > 0) {
+                r *= 10;
+                r += (num % 10);
+                num /= 10;
+            }
+            return r;
         }
     }
 
